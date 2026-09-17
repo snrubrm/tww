22fab9980747ab099f34abde132d94b79ef80437
snrubrm <snrubrm@proton.me>
2026-09-17 20:36:20 +0100

    d_a_obj_Vds OK (USA)

--- include/d/actor/d_a_obj_Vds.h
+++ include/d/actor/d_a_obj_Vds.h
@@ -2,25 +2,28 @@
 #define D_A_OBJ_VDS_H
 
 #include "f_op/f_op_actor.h"
+#include "m_Do/m_Do_ext.h"
+#include "d/d_bg_w.h"
 
 class J3DAnmTransformKey;
 
 namespace daObjVds {
-    static void ds_search_switchCB(void*, void*);
+    static void* ds_search_switchCB(void*, void*);
 
     class Act_c : public fopAc_ac_c {
     public:
-        void is_switch() const {}
+        virtual ~Act_c() {}
+        inline BOOL is_switch() const;
     
-        void SetLoopJointAnimation(J3DAnmTransformKey*, J3DAnmTransformKey*, float, float);
-        void PlayLoopJointAnimation();
+        BOOL SetLoopJointAnimation(J3DAnmTransformKey*, J3DAnmTransformKey*, float, float);
+        BOOL PlayLoopJointAnimation();
         void set_first_process();
-        void search_switchCB(fopAc_ac_c*);
-        void process_off_init();
+        void* search_switchCB(fopAc_ac_c*);
+        BOOL process_off_init();
         void process_off_main();
-        void process_on_init();
+        BOOL process_on_init();
         void process_on_main();
-        void process_init(int);
+        BOOL process_init(int);
         void process_main();
         void process_common();
         void create_point_light(int, cXyz*);
@@ -28,8 +31,8 @@ namespace daObjVds {
         void delete_point_light();
         void Event_init();
         void Event_exe();
-        void solidHeapCB(fopAc_ac_c*);
-        void create_heap();
+        static BOOL solidHeapCB(fopAc_ac_c*);
+        bool create_heap();
         cPhs_State _create();
         bool _delete();
         void set_mtx();
@@ -37,7 +40,28 @@ namespace daObjVds {
         bool _draw();
     
     public:
-        /* Place member variables here */
+        static const char M_arcname[];
+        /* 0x294 */ request_of_phase_process_class mPhase;
+        /* 0x29C */ Mtx mBgMtx;
+        /* 0x2CC */ mDoExt_McaMorf* M_anm0;
+        /* 0x2D0 */ J3DAnmTransformKey* M_bck_data0;
+        /* 0x2D4 */ mDoExt_brkAnm mBrk0;
+        /* 0x2EC */ J3DAnmTevRegKey* M_brk_data0;
+        /* 0x2F0 */ mDoExt_McaMorf* M_anm1;
+        /* 0x2F4 */ J3DAnmTransformKey* M_bck_data1;
+        /* 0x2F8 */ mDoExt_brkAnm mBrk1;
+        /* 0x310 */ J3DAnmTevRegKey* M_brk_data1;
+        /* 0x314 */ dBgW* mpBgW;
+        /* 0x318 */ s32 mLoopAnimation;
+        /* 0x31C */ s32 mProcess;
+        /* 0x320 */ s32 mLightState;
+        /* 0x324 */ fpc_ProcID mSwitchId[2];
+        /* 0x32C */ f32 mIntensity[2];
+        /* 0x334 */ s16 mEventIdx;
+        /* 0x336 */ s16 mEventState;
+        /* 0x338 */ s16 mVdsEvent;
+        /* 0x33C */ LIGHT_INFLUENCE mLight[2];
+        /* 0x37C */ cXyz mLightPos[2];
     };
 };
 
--- src/d/actor/d_a_obj_Vds.cpp
+++ src/d/actor/d_a_obj_Vds.cpp
@@ -5,125 +5,316 @@
 
 #include "d/dolzel_rel.h" // IWYU pragma: keep
 #include "d/actor/d_a_obj_Vds.h"
+#include "d/actor/d_a_obj_swlight.h"
+#include "d/d_com_inf_game.h"
+#include "d/d_a_obj.h"
+#include "JSystem/JUtility/JUTAssert.h"
+#include "res/Object/Vds.h"
+
+namespace daObjVds {
+namespace {
+struct Attr_c {
+    Vec eyeOffset;
+    GXColorS10 color;
+    f32 power;
+    f32 unused[4];
+};
+const Attr_c L_attr = {{222.5f, 598.73f, 178.9f}, {1024, 1024, 1024, 0}, 2200.0f, {0.0f, 0.0f, 0.0f, 0.0f}};
+}
+}
+
+inline BOOL daObjVds::Act_c::is_switch() const {
+    int sw = daObj::PrmAbstract(this, 8, 0);
+    return dComIfGs_isSwitch(sw, home.roomNo);
+}
 
 /* 00000078-00000134       .text SetLoopJointAnimation__Q28daObjVds5Act_cFP18J3DAnmTransformKeyP18J3DAnmTransformKeyff */
-void daObjVds::Act_c::SetLoopJointAnimation(J3DAnmTransformKey*, J3DAnmTransformKey*, float, float) {
-    /* Nonmatching */
+BOOL daObjVds::Act_c::SetLoopJointAnimation(J3DAnmTransformKey* anm0, J3DAnmTransformKey* anm1, float speed, float morf) {
+    M_anm0->setAnm(anm0, J3DFrameCtrl::EMode_LOOP, morf, speed, 0.0f, -1.0f, NULL);
+    M_anm1->setAnm(anm1, J3DFrameCtrl::EMode_LOOP, morf, speed, 0.0f, -1.0f, NULL);
+    mLoopAnimation = 1;
+    return TRUE;
 }
 
 /* 00000134-00000188       .text PlayLoopJointAnimation__Q28daObjVds5Act_cFv */
-void daObjVds::Act_c::PlayLoopJointAnimation() {
-    /* Nonmatching */
+BOOL daObjVds::Act_c::PlayLoopJointAnimation() {
+    M_anm0->play(NULL, 0, 0);
+    M_anm1->play(NULL, 0, 0);
+    return TRUE;
 }
 
 /* 00000188-000001E8       .text set_first_process__Q28daObjVds5Act_cFv */
 void daObjVds::Act_c::set_first_process() {
-    /* Nonmatching */
+    process_init(is_switch() ? 1 : 0);
 }
 
 /* 000001E8-00000214       .text ds_search_switchCB__8daObjVdsFPvPv */
-void daObjVds::ds_search_switchCB(void*, void*) {
-    /* Nonmatching */
+void* daObjVds::ds_search_switchCB(void* actor, void* self) {
+    return static_cast<Act_c*>(self)->search_switchCB(static_cast<fopAc_ac_c*>(actor));
 }
 
 /* 00000214-000002B0       .text search_switchCB__Q28daObjVds5Act_cFP10fopAc_ac_c */
-void daObjVds::Act_c::search_switchCB(fopAc_ac_c*) {
-    /* Nonmatching */
+void* daObjVds::Act_c::search_switchCB(fopAc_ac_c* actor) {
+    cXyz left, right;
+    static s16 angle_data[2] = {0, 0};
+    // Eye-position constants retained by the original build.
+    static const f32 eyeOffsets[] = {-222.5f, 598.73f, 178.9f, 222.5f};
+    if (fopAc_IsActor(actor) && fopAcM_GetName(actor) == fpcNm_Obj_Swlight_e) {
+        for (int i = 0; i < 2; i++) {
+            if (mSwitchId[i] == fpcM_ERROR_PROCESS_ID_e) {
+                mSwitchId[i] = fopAcM_GetID(actor);
+                break;
+            }
+        }
+    }
+    return NULL;
 }
 
 /* 000002EC-000003C8       .text process_off_init__Q28daObjVds5Act_cFv */
-void daObjVds::Act_c::process_off_init() {
-    /* Nonmatching */
+BOOL daObjVds::Act_c::process_off_init() {
+    if (mBrk0.init(M_anm0->getModel()->getModelData(), M_brk_data0, TRUE, J3DFrameCtrl::EMode_NONE, 1.0f, 0, -1, true, 0)) {
+        mBrk0.setPlaySpeed(0.0f);
+        if (mBrk1.init(M_anm1->getModel()->getModelData(), M_brk_data1, TRUE, J3DFrameCtrl::EMode_NONE, 1.0f, 0, -1, true, 0)) {
+            mBrk1.setPlaySpeed(0.0f);
+            return TRUE;
+        }
+    }
+    return FALSE;
 }
 
 /* 000003C8-00000474       .text process_off_main__Q28daObjVds5Act_cFv */
 void daObjVds::Act_c::process_off_main() {
-    /* Nonmatching */
+    for (int i = 0; i < 2; i++) {
+        daObjSwlight::Act_c* actor = static_cast<daObjSwlight::Act_c*>(fopAcM_SearchByID(mSwitchId[i]));
+        mIntensity[i] = actor->mF30;
+    }
+    if (is_switch()) process_init(1);
 }
 
 /* 00000474-000004F0       .text process_on_init__Q28daObjVds5Act_cFv */
-void daObjVds::Act_c::process_on_init() {
-    /* Nonmatching */
+BOOL daObjVds::Act_c::process_on_init() {
+    if (SetLoopJointAnimation(M_bck_data0, M_bck_data1, 1.0f, 0.0f)) {
+        for (int i = 0; i < 2; i++) mIntensity[i] = 1.0f;
+        return TRUE;
+    }
+    return FALSE;
 }
 
 /* 000004F0-000004F4       .text process_on_main__Q28daObjVds5Act_cFv */
 void daObjVds::Act_c::process_on_main() {
-    /* Nonmatching */
+
 }
 
 /* 000004F4-000005C0       .text process_init__Q28daObjVds5Act_cFi */
-void daObjVds::Act_c::process_init(int) {
-    /* Nonmatching */
+BOOL daObjVds::Act_c::process_init(int process) {
+    typedef BOOL (Act_c::*InitFunc)();
+    static InitFunc init[] = {&Act_c::process_off_init, &Act_c::process_on_init};
+    if (process >= 0 && process < 2 && (this->*init[process])()) {
+        mProcess = process;
+        return TRUE;
+    }
+    return FALSE;
 }
 
 /* 000005C0-0000065C       .text process_main__Q28daObjVds5Act_cFv */
 void daObjVds::Act_c::process_main() {
-    /* Nonmatching */
+    typedef void (Act_c::*MainFunc)();
+    static MainFunc main[] = {&Act_c::process_off_main, &Act_c::process_on_main};
+    if (mProcess >= 0 && mProcess < 2) (this->*main[mProcess])();
 }
 
 /* 0000065C-000007EC       .text process_common__Q28daObjVds5Act_cFv */
 void daObjVds::Act_c::process_common() {
-    /* Nonmatching */
+    switch (mLightState) {
+    case 0:
+        for (int i = 0; i < 2; i++) mSwitchId[i] = fpcM_ERROR_PROCESS_ID_e;
+        fopAcIt_Judge(ds_search_switchCB, this);
+        if (mSwitchId[0] != fpcM_ERROR_PROCESS_ID_e && mSwitchId[1] != fpcM_ERROR_PROCESS_ID_e) {
+            fopAc_ac_c* left = fopAcM_SearchByID(mSwitchId[0]);
+            fopAc_ac_c* right = fopAcM_SearchByID(mSwitchId[1]);
+            if (left != NULL && right != NULL) {
+                if ((s16)(left->shape_angle.y - shape_angle.y) >= 0) {
+                    fpc_ProcID id = mSwitchId[0];
+                    mSwitchId[0] = mSwitchId[1];
+                    mSwitchId[1] = id;
+                }
+                mLightState = 1;
+            }
+        }
+        break;
+    case 1: {
+        fopAc_ac_c* left = fopAcM_SearchByID(mSwitchId[0]);
+        fopAc_ac_c* right = fopAcM_SearchByID(mSwitchId[1]);
+        if (left != NULL && right != NULL) {
+            create_point_light(0, &left->current.pos);
+            create_point_light(1, &right->current.pos);
+            mLightState = 2;
+        }
+        break;
+    }
+    case 2:
+        execute_point_light();
+        break;
+    }
 }
 
 /* 000007EC-0000087C       .text create_point_light__Q28daObjVds5Act_cFiP4cXyz */
-void daObjVds::Act_c::create_point_light(int, cXyz*) {
-    /* Nonmatching */
+void daObjVds::Act_c::create_point_light(int index, cXyz* pos) {
+    u8 i = index & 1;
+    mLight[i].mPos = *pos;
+    mLightPos[i] = *pos;
+    mLight[i].mColor.r = 1024;
+    mLight[i].mColor.g = 1024;
+    mLight[i].mColor.b = 1024;
+    mLight[i].mPower = 0.0f;
+    mLight[i].mFluctuation = 0.0f;
+    dKy_plight_set(&mLight[i]);
 }
 
 /* 0000087C-00000900       .text execute_point_light__Q28daObjVds5Act_cFv */
 void daObjVds::Act_c::execute_point_light() {
-    /* Nonmatching */
+    for (int i = 0; i < 2; i++) {
+        mLight[i].mPower = 2200.0f * mIntensity[i];
+        cXyz pos(mLightPos[i].x, mLightPos[i].y, mLightPos[i].z);
+        mLight[i].mPos = pos;
+        mLight[i].mColor.r = 1024;
+        mLight[i].mColor.g = 1024;
+        mLight[i].mColor.b = 1024;
+    }
 }
 
 /* 00000900-00000954       .text delete_point_light__Q28daObjVds5Act_cFv */
 void daObjVds::Act_c::delete_point_light() {
-    /* Nonmatching */
+    for (int i = 0; i < 2; i++) dKy_plight_cut(&mLight[i]);
 }
 
 /* 00000954-00000968       .text Event_init__Q28daObjVds5Act_cFv */
 void daObjVds::Act_c::Event_init() {
-    /* Nonmatching */
+    mEventIdx = -1;
+    mEventState = 0;
 }
 
 /* 00000968-00000A28       .text Event_exe__Q28daObjVds5Act_cFv */
 void daObjVds::Act_c::Event_exe() {
-    /* Nonmatching */
+    switch (mEventState) {
+    case 1:
+        if (eventInfo.checkCommandDemoAccrpt()) mEventState = 2;
+        else {
+            fopAcM_orderOtherEventId(this, mEventIdx);
+            eventInfo.onCondition(2);
+        }
+        break;
+    case 2:
+        if (dComIfGp_evmng_endCheck(mEventIdx)) {
+            dComIfGp_event_reset();
+            Event_init();
+        }
+        break;
+    }
 }
 
 /* 00000A28-00000A4C       .text solidHeapCB__Q28daObjVds5Act_cFP10fopAc_ac_c */
-void daObjVds::Act_c::solidHeapCB(fopAc_ac_c*) {
-    /* Nonmatching */
+BOOL daObjVds::Act_c::solidHeapCB(fopAc_ac_c* actor) {
+    return static_cast<Act_c*>(actor)->create_heap();
 }
 
+const char daObjVds::Act_c::M_arcname[] = "Vds";
+
 /* 00000A4C-00001020       .text create_heap__Q28daObjVds5Act_cFv */
-void daObjVds::Act_c::create_heap() {
-    /* Nonmatching */
+bool daObjVds::Act_c::create_heap() {
+    J3DModelData* mdl_data0 = (J3DModelData*)dComIfG_getObjectRes(M_arcname, dRes_INDEX_VDS_BDL_VDSWT0_e);
+    JUT_ASSERT(848, mdl_data0 != 0);
+    M_bck_data0 = (J3DAnmTransformKey*)dComIfG_getObjectRes(M_arcname, dRes_INDEX_VDS_BCK_VDSWT0_e);
+    JUT_ASSERT(852, M_bck_data0 != 0);
+    if (mdl_data0 != NULL && M_bck_data0 != NULL) {
+        M_anm0 = new mDoExt_McaMorf(mdl_data0, NULL, NULL, M_bck_data0, 0, 1.0f, 0, -1, TRUE, NULL, 0, 0x11020203);
+    }
+    JUT_ASSERT(865, M_anm0 != 0);
+    J3DModelData* mdl_data1 = (J3DModelData*)dComIfG_getObjectRes(M_arcname, dRes_INDEX_VDS_BDL_VDSWT1_e);
+    JUT_ASSERT(869, mdl_data1 != 0);
+    M_bck_data1 = (J3DAnmTransformKey*)dComIfG_getObjectRes(M_arcname, dRes_INDEX_VDS_BCK_VDSWT1_e);
+    JUT_ASSERT(873, M_bck_data1 != 0);
+    if (mdl_data1 != NULL && M_bck_data1 != NULL) {
+        M_anm1 = new mDoExt_McaMorf(mdl_data1, NULL, NULL, M_bck_data1, 0, 1.0f, 0, -1, TRUE, NULL, 0, 0x11020203);
+    }
+    JUT_ASSERT(886, M_anm1 != 0);
+    M_brk_data0 = (J3DAnmTevRegKey*)dComIfG_getObjectRes(M_arcname, dRes_INDEX_VDS_BRK_VDSWT0_e);
+    JUT_ASSERT(891, M_brk_data0 != 0);
+    BOOL brk0 = mBrk0.init(mdl_data0, M_brk_data0, TRUE, J3DFrameCtrl::EMode_NONE, 1.0f, 0, -1, false, 0);
+    M_brk_data1 = (J3DAnmTevRegKey*)dComIfG_getObjectRes(M_arcname, dRes_INDEX_VDS_BRK_VDSWT1_e);
+    JUT_ASSERT(904, M_brk_data1 != 0);
+    BOOL brk1 = mBrk1.init(mdl_data1, M_brk_data1, TRUE, J3DFrameCtrl::EMode_NONE, 1.0f, 0, -1, false, 0);
+    set_mtx();
+    cBgD_t* bgw_data = (cBgD_t*)dComIfG_getObjectRes(M_arcname, dRes_INDEX_VDS_DZB_VDSWT_e);
+    JUT_ASSERT(926, bgw_data != 0);
+    if (bgw_data != NULL) {
+        mpBgW = new dBgW;
+        if (mpBgW != NULL) mpBgW->Set(bgw_data, cBgW::MOVE_BG_e, &mBgMtx);
+    }
+    return (((((((((M_bck_data0 != NULL && M_anm0 != NULL) && M_anm0->getModel() != NULL) && M_bck_data1 != NULL) && M_anm1 != NULL) && M_anm1->getModel() != NULL) && mpBgW != NULL) && M_brk_data0 != NULL) && M_brk_data1 != NULL) && brk0) && brk1;
 }
 
 /* 00001020-000011EC       .text _create__Q28daObjVds5Act_cFv */
 cPhs_State daObjVds::Act_c::_create() {
-    /* Nonmatching */
+    fopAcM_SetupActor(this, Act_c);
+    cPhs_State phase = dComIfG_resLoad(&mPhase, M_arcname);
+    if (phase == cPhs_COMPLEATE_e) {
+        if (fopAcM_entrySolidHeap(this, solidHeapCB, 0)) {
+            set_first_process();
+            fopAcM_SetMtx(this, M_anm0->getModel()->getBaseTRMtx());
+            fopAcM_setCullSizeBox(this, -2000.0f, -2000.0f, -2000.0f, 2000.0f, 2000.0f, 2000.0f);
+            dComIfG_Bgsp()->Regist(mpBgW, this);
+            mpBgW->SetCrrFunc(NULL);
+            Event_init();
+            mVdsEvent = dComIfGp_evmng_getEventIdx("Vds");
+            for (int i = 0; i < 2; i++) mSwitchId[i] = fpcM_ERROR_PROCESS_ID_e;
+        } else phase = cPhs_ERROR_e;
+    }
+    return phase;
 }
 
 /* 000012D4-00001368       .text _delete__Q28daObjVds5Act_cFv */
 bool daObjVds::Act_c::_delete() {
-    /* Nonmatching */
+    if (heap != NULL && mpBgW != NULL && mpBgW->ChkUsed()) dComIfG_Bgsp()->Release(mpBgW);
+    delete_point_light();
+    dComIfG_resDelete(&mPhase, M_arcname);
+    return true;
 }
 
 /* 00001368-00001420       .text set_mtx__Q28daObjVds5Act_cFv */
 void daObjVds::Act_c::set_mtx() {
-    /* Nonmatching */
+    M_anm0->getModel()->setBaseScale(scale);
+    mDoMtx_stack_c::transS(current.pos);
+    mDoMtx_stack_c::ZXYrotM(shape_angle);
+    M_anm0->getModel()->setBaseTRMtx(mDoMtx_stack_c::get());
+    M_anm1->getModel()->setBaseTRMtx(mDoMtx_stack_c::get());
+    MTXCopy(mDoMtx_stack_c::get(), mBgMtx);
 }
 
 /* 00001420-000014EC       .text _execute__Q28daObjVds5Act_cFv */
 bool daObjVds::Act_c::_execute() {
-    /* Nonmatching */
+    Event_exe();
+    PlayLoopJointAnimation();
+    mBrk0.setFrame(mIntensity[0] * (M_brk_data0->getFrameMax() - 1));
+    mBrk1.setFrame(mIntensity[1] * (M_brk_data1->getFrameMax() - 1));
+    process_common();
+    process_main();
+    set_mtx();
+    if (mpBgW != NULL) mpBgW->Move();
+    return true;
 }
 
 /* 000014EC-000015A8       .text _draw__Q28daObjVds5Act_cFv */
 bool daObjVds::Act_c::_draw() {
-    /* Nonmatching */
+    GXColor color = {255, 0, 0, 128};
+    g_env_light.settingTevStruct(TEV_TYPE_BG0, &current.pos, &tevStr);
+    g_env_light.setLightTevColorType(M_anm0->getModel(), &tevStr);
+    g_env_light.setLightTevColorType(M_anm1->getModel(), &tevStr);
+    mBrk0.entry(M_anm0->getModel()->getModelData());
+    mBrk1.entry(M_anm1->getModel()->getModelData());
+    M_anm0->updateDL();
+    M_anm1->updateDL();
+    return true;
 }
 
 namespace daObjVds {
