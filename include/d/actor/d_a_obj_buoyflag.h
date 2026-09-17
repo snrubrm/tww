31ae1474a153e039e1755afcb6c09b8d3c289943
snrubrm <snrubrm@proton.me>
2026-09-17 21:57:17 +0100

    d_a_obj_buoyflag: Implement buoy flag (USA partial)

--- include/d/actor/d_a_obj_buoyflag.h
+++ include/d/actor/d_a_obj_buoyflag.h
@@ -2,12 +2,15 @@
 #define D_A_OBJ_BUOYFLAG_H
 
 #include "f_op/f_op_actor.h"
+#include "d/d_cc_d.h"
+#include "d/d_a_obj.h"
+#include "JSystem/J3DGraphBase/J3DPacket.h"
 #include "m_Do/m_Do_mtx.h"
 #include "f_op/f_op_actor_mng.h"
 
 namespace daObjBuoyflag {
     class Act_c;
-    
+
     enum Type_e {
         Type_UNK0_e = 0,
         Type_UNK1_e = 1,
@@ -17,37 +20,58 @@ namespace daObjBuoyflag {
         Texture_00_e = 0x0,
         Texture_01_e = 0x1,
     };
-    
-    class Packet_c {
-    public:
-        void calc_pos_gravity(int, int) {}
-        void calc_pos_spd(int, int) {}
-        void calc_pos_spring(int, int) {}
-        void calc_pos_wave(int, int) {}
-        
-        void hasi_nrm_init();
-        void draw_hata(daObjBuoyflag::Act_c*);
-        void draw_hasi(daObjBuoyflag::Act_c*);
-        void draw();
-        void init(daObjBuoyflag::Act_c*);
-        void calc_wind_base(daObjBuoyflag::Act_c*);
-        void calc_pos_spring_near(const cXyz*, const cXyz*, float, float);
-        void calc_pos(daObjBuoyflag::Act_c*);
-        void calc_nrm();
-        void calc(daObjBuoyflag::Act_c*);
-        void update(daObjBuoyflag::Act_c*);
+
+    struct AttrType_c {
+        f32 scale;
+        bool collision;
+        bool flag;
     };
-    
+
     class DrawVtx_c {
     public:
-        
+        cXyz pos[5][7];
+        cXyz normal[5][7];
+        cXyz backNormal[5][7];
     };
-    
+
     class MoveVtx_c {
     public:
-        
+        cXyz speed[5][7];
     };
-    
+
+    class Packet_c : public J3DPacket {
+    public:
+        Packet_c() { hasi_nrm_init(); }
+        void calc_pos_gravity(int, int);
+        void calc_pos_spd(int, int);
+        void calc_pos_spring(int, int);
+        void calc_pos_wave(int, int);
+        static void hasi_nrm_init();
+        void draw_hata(Act_c*);
+        void draw_hasi(Act_c*);
+        virtual void draw();
+        void init(Act_c*);
+        void calc_wind_base(Act_c*);
+        void calc_pos_spring_near(const cXyz*, const cXyz*, float, float);
+        void calc_pos(Act_c*);
+        void calc_nrm();
+        void calc(Act_c*);
+        void update(Act_c*);
+        static cXyz M_hasi_nrm[11];
+        static bool M_hasi_nrm_flag;
+        /* 0x010 */ DrawVtx_c mDraw[2];
+        /* 0x9E8 */ MoveVtx_c mMove;
+        /* 0xB8C */ int mBuffer;
+        /* 0xB90 */ Mtx mPoleMtx;
+        /* 0xBC0 */ Mtx mFlagMtx;
+        /* 0xBF0 */ Mtx mPoleViewMtx;
+        /* 0xC20 */ Mtx mFlagViewMtx;
+        /* 0xC50 */ cXyz mGravity;
+        /* 0xC5C */ cXyz mWind;
+        /* 0xC68 */ s16 mPhase[12];
+        /* 0xC80 */ cXyz mForce;
+    };
+
     class Act_c : public fopAc_ac_c {
     public:
         static fpc_ProcID make_norm(fpc_ProcID parentPId, cXyz* pos, int roomNo, csXyz* angle) {
@@ -58,7 +82,7 @@ namespace daObjBuoyflag {
             s32 b = arg2 ? 1 : 0;
             return (type << 0) | (texture << 8) | (b << 31);
         }
-    
+
         void setup(MtxP mtx) { cMtx_copy(mtx, m1090); }
         inline cPhs_State _create();
         inline bool _delete();
@@ -73,24 +97,39 @@ namespace daObjBuoyflag {
             u32 params = prm_make(Type_UNK1_e, texture, false);
             return fopAcM_createChild(fpcNm_Obj_Buoyflag_e, parent, params, pos, room, angle);
         }
-        void attr_type() const; // weak but not inlined?
-        void prm_get_noCull() const {}
-        void prm_get_texture() const; // weak but not inlined?
-        void prm_get_type() const {}
-    
+        const AttrType_c& attr_type() const;
+        int prm_get_noCull() const { return daObj::PrmAbstract(this, PRM_NOCULL_W, PRM_NOCULL_S); }
+        int prm_get_texture() const;
+        int prm_get_type() const { return daObj::PrmAbstract(this, PRM_TYPE_W, PRM_TYPE_S); }
+
         void mtx_init();
-        void mode_afl();
-        void mode_jumpToSea();
-    
+        bool mode_afl();
+        bool mode_jumpToSea();
+
     public:
-        /* 0x0290 */ u8 m0290[0x1090 - 0x0290];
+        enum Prm_e {
+            PRM_TYPE_W = 2, PRM_TYPE_S = 0,
+            PRM_TEXTURE_W = 1, PRM_TEXTURE_S = 8,
+            PRM_NOCULL_W = 1, PRM_NOCULL_S = 31,
+        };
+        static const dCcD_SrcCyl M_cyl_src;
+        static const AttrType_c M_attr_type[4];
+        /* 0x0290 */ request_of_phase_process_class mPhase;
+        /* 0x0298 */ dCcD_Stts mStts;
+        /* 0x02D4 */ dCcD_Cyl mCyl;
+        /* 0x0404 */ Packet_c mPacket;
         /* 0x1090 */ Mtx m1090;
-        /* 0x10C0 */ u8 m10C0[0x10F0 - 0x10C0];
+        /* 0x10C0 */ Mtx m10C0;
         /* 0x10F0 */ Mtx m10F0;
-        /* 0x1120 */ u8 m1120[0x1124 - 0x1120];
+        /* 0x1120 */ int mType;
         /* 0x1124 */ s32 m1124;
         /* 0x1128 */ bool m1128;
+        /* 0x112A */ s16 mTilt;
+        /* 0x112C */ f32 mTiltSpeed;
+        /* 0x1130 */ cXyz mTiltAxis;
     };
 };
+
+STATIC_ASSERT(sizeof(daObjBuoyflag::Act_c) == 0x113C);
 
 #endif /* D_A_OBJ_BUOYFLAG_H */
