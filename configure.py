6dcb061409f1076145c72530812bfd9197557857
snrubrm <snrubrm@proton.me>
2026-09-17 19:00:52 +0100

    d_a_swpropeller OK (USA)

--- configure.py
+++ configure.py
@@ -1823,7 +1823,7 @@ config.libs = [
     ActorRel(Matching,    "d_a_steam_tag"),
     ActorRel(Matching,    "d_a_swattack"),
     ActorRel(Matching,    "d_a_switem"),
-    ActorRel(NonMatching, "d_a_swpropeller"),
+    ActorRel(MatchingFor("GZLE01"), "d_a_swpropeller"),
     ActorRel(Matching,    "d_a_swtact"),
     ActorRel(Matching,    "d_a_tag_etc"),
     ActorRel(Matching,    "d_a_tag_island"),
--- include/d/actor/d_a_swpropeller.h
+++ include/d/actor/d_a_swpropeller.h
@@ -8,7 +8,7 @@
 class daSwProp_c : public fopAc_ac_c {
 public:
     bool _delete();
-    void CreateHeap();
+    BOOL CreateHeap();
     void CreateInit();
     cPhs_State _create();
     void set_mtx();
@@ -16,19 +16,23 @@ public:
     bool _draw();
 
 public:
-    /* 0x290 */ u8 m290[0x294 - 0x290];
-    /* 0x294 */ void* m294;
+    static const char* m_arcname[2];
+    static const s16 m_bdlidx[2];
+    static const u32 m_heapsize[2];
+    /* 0x290 */ request_of_phase_process_class mPhs;
     /* 0x298 */ J3DModel* mpModel;
     /* 0x29C */ dCcD_Stts mStts;
     /* 0x2D8 */ dCcD_Cyl mCyl;
     /* 0x408 */ u8 m408[0x43C - 0x408];
-    /* 0x43C */ dBgS_Acch mAcch;
+    /* 0x43C */ dBgS_ObjAcch mAcch;
     /* 0x600 */ dBgS_AcchCir mAcchCir;
     /* 0x640 */ s16 mRotY;
     /* 0x642 */ s16 mRotYVel;
-    /* 0x644 */ u8 m644[0x648 - 0x644];
+    /* 0x644 */ s16 mTargetRotYVel;
     /* 0x648 */ u32 m648;
-    /* 0x64C */ u8 m64C[0x800 - 0x64C];
+    /* 0x64C */ u8 mWindHit;
+    /* 0x64D */ u8 mType;
+    /* 0x64E */ u8 mRebounding;
 };
 
 #endif /* D_A_SWPROPELLER_H */
