d2a93909aa905672e7ea23fe7378f16230c3bc5f
snrubrm <snrubrm@proton.me>
2026-09-17 18:57:32 +0100

    d_a_obj_ashut OK (USA)

--- include/d/actor/d_a_obj_ashut.h
+++ include/d/actor/d_a_obj_ashut.h
@@ -6,13 +6,12 @@
 namespace daObjAshut {
     class Act_c : public dBgS_MoveBgActor {
     public:
-        enum Mode_e {
-            
-        };
+        enum Mode_e { Mode_UPPER, Mode_U_L, Mode_LOWER, Mode_L_U, Mode_DEMOREQ, Mode_NONE };
+        enum Prm_e { PRM_SWSAVE_W = 8, PRM_SWSAVE_S = 0, PRM_EVID_W = 8, PRM_EVID_S = 8 };
     
-        void is_switch() const {}
-        void prm_get_evId() const {}
-        void prm_get_swSave() const {}
+        BOOL is_switch() const;
+        u8 prm_get_evId() const { return daObj::PrmAbstract(this, PRM_EVID_W, PRM_EVID_S); }
+        s32 prm_get_swSave() const { return daObj::PrmAbstract(this, PRM_SWSAVE_W, PRM_SWSAVE_S); }
     
         virtual BOOL CreateHeap();
         virtual BOOL Create();
@@ -21,7 +20,7 @@ namespace daObjAshut {
         BOOL Mthd_Delete();
         void set_mtx();
         void init_mtx();
-        void chk_safe_area() const;
+        bool chk_safe_area() const;
         void mode_upper_init();
         void mode_upper();
         void mode_u_l_init();
@@ -36,7 +35,18 @@ namespace daObjAshut {
         virtual BOOL Draw();
     
     public:
-        /* Place member variables here */
+        static const char M_arcname[];
+        static Mtx M_tmp_mtx;
+        /* 0x2C8 */ request_of_phase_process_class mPhs;
+        /* 0x2D0 */ J3DModel* mModel;
+        /* 0x2D4 */ Mode_e mMode;
+        /* 0x2D8 */ f32 mOffset;
+        /* 0x2DC */ f32 mSpeed;
+        /* 0x2E0 */ u8 mBounces;
+        /* 0x2E2 */ s16 mTimer;
+        /* 0x2E4 */ bool mDemoAccepted;
+        /* 0x2E6 */ s16 mEventIdx;
+        /* 0x2E8 */ Mode_e mDemoNext;
     };
 };
 
--- src/d/actor/d_a_obj_ashut.cpp
+++ src/d/actor/d_a_obj_ashut.cpp
@@ -5,105 +5,245 @@
 
 #include "d/dolzel_rel.h" // IWYU pragma: keep
 #include "d/actor/d_a_obj_ashut.h"
+#include "d/d_com_inf_game.h"
+#include "f_op/f_op_actor_mng.h"
+#include "JSystem/JUtility/JUTAssert.h"
+#include "res/Object/Ashut.h"
+
+namespace daObjAshut {
+namespace {
+struct Attr_c {
+    f32 height;
+    f32 fallAccel;
+    f32 fallDrag;
+    u8 fallBounces;
+    f32 fallBounce;
+    f32 fallMaxBounce;
+    f32 riseAccel;
+    f32 riseDrag;
+    f32 riseSlowDrag;
+    u8 riseTime;
+    u8 riseSlowTime;
+    u8 riseBounces;
+    f32 riseBounce;
+    f32 riseMaxBounce;
+};
+static const Attr_c L_attr = {
+    250.0f, -2.5f, 0.05f, 5, -0.5f, 7.0f,
+    0.8f, 0.1f, 0.4f, 14, 4, 5, -0.6f, -4.0f,
+};
+inline const Attr_c& attr() { return L_attr; }
+}
+}
+
+const char daObjAshut::Act_c::M_arcname[] = "Ashut";
+Mtx daObjAshut::Act_c::M_tmp_mtx;
+
+inline BOOL daObjAshut::Act_c::is_switch() const {
+    s32 sw = prm_get_swSave();
+    return dComIfGs_isSwitch(sw, home.roomNo);
+}
 
 /* 00000078-0000012C       .text CreateHeap__Q210daObjAshut5Act_cFv */
 BOOL daObjAshut::Act_c::CreateHeap() {
-    /* Nonmatching */
+    J3DModelData* mdl_data = (J3DModelData*)dComIfG_getObjectRes(M_arcname, dRes_INDEX_ASHUT_BDL_ASHUT_e);
+    JUT_ASSERT(249, mdl_data != NULL);
+    mModel = mDoExt_J3DModel__create(mdl_data, 0x80000, 0x11000022);
+    return mModel != NULL;
 }
 
 /* 0000012C-00000224       .text Create__Q210daObjAshut5Act_cFv */
 BOOL daObjAshut::Act_c::Create() {
-    /* Nonmatching */
+    mpBgW->SetPriority((cBgW::PRIORITY)1);
+    fopAcM_SetMtx(this, mModel->getBaseTRMtx());
+    init_mtx();
+    fopAcM_setCullSizeBox(this, -80.0f, -5.0f, -30.0f, 80.0f, 250.0f, 30.0f);
+    mDemoAccepted = false;
+    mEventIdx = dComIfGp_evmng_getEventIdx(NULL, prm_get_evId());
+    mDemoNext = Mode_NONE;
+    if (is_switch()) mode_upper_init();
+    else mode_lower_init();
+    return TRUE;
 }
 
 /* 00000224-00000388       .text Mthd_Create__Q210daObjAshut5Act_cFv */
 cPhs_State daObjAshut::Act_c::Mthd_Create() {
-    /* Nonmatching */
+    fopAcM_SetupActor(this, Act_c);
+    cPhs_State phase_state = dComIfG_resLoad(&mPhs, M_arcname);
+    if (phase_state == cPhs_COMPLEATE_e) {
+        phase_state = MoveBGCreate(M_arcname, dRes_INDEX_ASHUT_DZB_ASHUT_e, NULL, 0x760);
+        JUT_ASSERT(312, (phase_state == cPhs_COMPLEATE_e) || (phase_state == cPhs_ERROR_e));
+        if (is_switch() && mpBgW->ChkUsed()) dComIfG_Bgsp()->Release(mpBgW);
+    }
+    return phase_state;
 }
 
 /* 00000388-00000390       .text Delete__Q210daObjAshut5Act_cFv */
 BOOL daObjAshut::Act_c::Delete() {
-    /* Nonmatching */
+    return TRUE;
 }
 
 /* 00000390-000003DC       .text Mthd_Delete__Q210daObjAshut5Act_cFv */
 BOOL daObjAshut::Act_c::Mthd_Delete() {
-    /* Nonmatching */
+    BOOL ret = MoveBGDelete();
+    dComIfG_resDelete(&mPhs, M_arcname);
+    return ret;
 }
 
 /* 000003DC-0000046C       .text set_mtx__Q210daObjAshut5Act_cFv */
 void daObjAshut::Act_c::set_mtx() {
-    /* Nonmatching */
+    mDoMtx_stack_c::transS(current.pos);
+    mDoMtx_stack_c::ZXYrotM(shape_angle);
+    MTXCopy(mDoMtx_stack_c::get(), M_tmp_mtx);
+    mDoMtx_stack_c::get()[1][3] += mOffset;
+    mModel->setBaseTRMtx(mDoMtx_stack_c::get());
 }
 
 /* 0000046C-000004A8       .text init_mtx__Q210daObjAshut5Act_cFv */
 void daObjAshut::Act_c::init_mtx() {
-    /* Nonmatching */
+    mModel->setBaseScale(scale);
+    set_mtx();
 }
 
 /* 000004A8-000005A4       .text chk_safe_area__Q210daObjAshut5Act_cCFv */
-void daObjAshut::Act_c::chk_safe_area() const {
-    /* Nonmatching */
+bool daObjAshut::Act_c::chk_safe_area() const {
+    s16 angle = -shape_angle.y;
+    fopAc_ac_c* player = dComIfGp_getPlayer(0);
+    cXyz delta = player->current.pos - current.pos;
+    cXyz local;
+    mDoMtx_stack_c::YrotS(angle);
+    mDoMtx_stack_c::multVecSR(&delta, &local);
+    return (std::fabsf(local.y) < 100.0f && std::fabsf(local.z) < 55.0f) && std::fabsf(local.x) < 105.0f;
 }
 
 /* 000005A4-000005BC       .text mode_upper_init__Q210daObjAshut5Act_cFv */
 void daObjAshut::Act_c::mode_upper_init() {
-    /* Nonmatching */
+    mMode = Mode_UPPER;
+    mOffset = attr().height;
 }
 
 /* 000005BC-0000066C       .text mode_upper__Q210daObjAshut5Act_cFv */
 void daObjAshut::Act_c::mode_upper() {
-    /* Nonmatching */
+    if (!is_switch() && !chk_safe_area()) {
+        if (!mpBgW->ChkUsed()) dComIfG_Bgsp()->Regist(mpBgW, this);
+        mode_demoreq_init(Mode_U_L);
+    }
 }
 
 /* 0000066C-00000700       .text mode_u_l_init__Q210daObjAshut5Act_cFv */
 void daObjAshut::Act_c::mode_u_l_init() {
-    /* Nonmatching */
+    mMode = Mode_U_L;
+    mSpeed = 0.0f;
+    mBounces = attr().fallBounces;
+    mDoAud_seStart(JA_SE_OBJ_P_SHIP_SHTR_CL, &eyePos, 0, dComIfGp_getReverb(current.roomNo));
 }
 
 /* 00000700-000007C0       .text mode_u_l__Q210daObjAshut5Act_cFv */
 void daObjAshut::Act_c::mode_u_l() {
-    /* Nonmatching */
+    mSpeed += attr().fallAccel;
+    mSpeed *= 1.0f - attr().fallDrag;
+    mOffset += mSpeed;
+    if (mOffset <= 0.0f) {
+        if (mBounces == 0) mode_lower_init();
+        else {
+            mBounces--;
+            mOffset *= attr().fallBounce;
+            mSpeed *= attr().fallBounce;
+            if (mSpeed > attr().fallMaxBounce) mSpeed = attr().fallMaxBounce;
+        }
+    }
 }
 
 /* 000007C0-000007D8       .text mode_lower_init__Q210daObjAshut5Act_cFv */
 void daObjAshut::Act_c::mode_lower_init() {
-    /* Nonmatching */
+    mMode = Mode_LOWER;
+    mOffset = 0.0f;
 }
 
 /* 000007D8-00000838       .text mode_lower__Q210daObjAshut5Act_cFv */
 void daObjAshut::Act_c::mode_lower() {
-    /* Nonmatching */
+    if (is_switch()) mode_demoreq_init(Mode_L_U);
 }
 
 /* 00000838-000008D4       .text mode_l_u_init__Q210daObjAshut5Act_cFv */
 void daObjAshut::Act_c::mode_l_u_init() {
-    /* Nonmatching */
+    mMode = Mode_L_U;
+    mSpeed = 0.0f;
+    mBounces = attr().riseBounces;
+    mTimer = attr().riseTime;
+    mDoAud_seStart(JA_SE_OBJ_P_SHIP_SHTR_OP, &eyePos, 0, dComIfGp_getReverb(current.roomNo));
 }
 
 /* 000008D4-00000A50       .text mode_l_u__Q210daObjAshut5Act_cFv */
 void daObjAshut::Act_c::mode_l_u() {
-    /* Nonmatching */
+    if (mTimer > 0) mTimer--;
+    mSpeed += attr().riseAccel;
+    if (mTimer > 0 && mTimer < attr().riseSlowTime) mSpeed *= 1.0f - attr().riseSlowDrag;
+    else mSpeed *= 1.0f - attr().riseDrag;
+    mOffset += mSpeed;
+    if (mOffset > 150.0f && mpBgW->ChkUsed()) dComIfG_Bgsp()->Release(mpBgW);
+    if (mOffset >= attr().height) {
+        if (mBounces == 0) mode_upper_init();
+        else {
+            mBounces--;
+            mOffset = attr().height + (mOffset - attr().height) * attr().riseBounce;
+            mSpeed *= attr().riseBounce;
+            if (mSpeed < attr().riseMaxBounce) mSpeed = attr().riseMaxBounce;
+        }
+    }
 }
 
 /* 00000A50-00000B0C       .text mode_demoreq_init__Q210daObjAshut5Act_cFQ310daObjAshut5Act_c6Mode_e */
-void daObjAshut::Act_c::mode_demoreq_init(daObjAshut::Act_c::Mode_e) {
-    /* Nonmatching */
+void daObjAshut::Act_c::mode_demoreq_init(daObjAshut::Act_c::Mode_e i_demo_next) {
+    JUT_ASSERT(546, (i_demo_next == Mode_U_L) || (i_demo_next == Mode_L_U));
+    if (mDemoAccepted) {
+        if (i_demo_next == Mode_U_L) mode_u_l_init();
+        else mode_l_u_init();
+    } else {
+        mMode = Mode_DEMOREQ;
+        mDemoNext = i_demo_next;
+    }
 }
 
 /* 00000B0C-00000BE4       .text mode_demoreq__Q210daObjAshut5Act_cFv */
 void daObjAshut::Act_c::mode_demoreq() {
-    /* Nonmatching */
+    bool start = false;
+    if (dComIfGp_evmng_existence(mEventIdx)) {
+        if (eventInfo.checkCommandDemoAccrpt()) {
+            start = true;
+            mDemoAccepted = true;
+        } else {
+            fopAcM_orderOtherEventId(this, mEventIdx, prm_get_evId());
+            eventInfo.onCondition(dEvtCnd_UNK2_e);
+        }
+    } else {
+        start = true;
+    }
+    if (start) {
+        if (mDemoNext == Mode_U_L) mode_u_l_init();
+        else mode_l_u_init();
+    }
 }
 
 /* 00000BE4-00000D0C       .text Execute__Q210daObjAshut5Act_cFPPA3_A4_f */
-BOOL daObjAshut::Act_c::Execute(Mtx**) {
-    /* Nonmatching */
+BOOL daObjAshut::Act_c::Execute(Mtx** mtx) {
+    static void (Act_c::*mode_proc[])() = {
+        &Act_c::mode_upper, &Act_c::mode_u_l, &Act_c::mode_lower,
+        &Act_c::mode_l_u, &Act_c::mode_demoreq,
+    };
+    (this->*mode_proc[mMode])();
+    eyePos.set(current.pos.x, current.pos.y + mOffset, current.pos.z);
+    set_mtx();
+    *mtx = &M_tmp_mtx;
+    return TRUE;
 }
 
 /* 00000D0C-00000D6C       .text Draw__Q210daObjAshut5Act_cFv */
 BOOL daObjAshut::Act_c::Draw() {
-    /* Nonmatching */
+    g_env_light.settingTevStruct(TEV_TYPE_BG0, &current.pos, &tevStr);
+    g_env_light.setLightTevColorType(mModel, &tevStr);
+    mDoExt_modelUpdateDL(mModel);
+    return TRUE;
 }
 
 namespace daObjAshut {
@@ -125,12 +265,12 @@ BOOL Mthd_Execute(void* i_this) {
 
 /* 00000DCC-00000DF8       .text Mthd_Draw__Q210daObjAshut27@unnamed@d_a_obj_ashut_cpp@FPv */
 BOOL Mthd_Draw(void* i_this) {
-    return ((daObjAshut::Act_c*)i_this)->Draw();
+    return ((daObjAshut::Act_c*)i_this)->MoveBGDraw();
 }
 
 /* 00000DF8-00000E24       .text Mthd_IsDelete__Q210daObjAshut27@unnamed@d_a_obj_ashut_cpp@FPv */
 BOOL Mthd_IsDelete(void* i_this) {
-    return ((daObjAshut::Act_c*)i_this)->IsDelete();
+    return ((daObjAshut::Act_c*)i_this)->MoveBGIsDelete();
 }
 
 static actor_method_class Mthd_Table = {
