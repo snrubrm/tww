29f6ba6215947cf980ebb3cbf8127ff3f590f585
snrubrm <snrubrm@proton.me>
2026-09-17 19:43:21 +0100

    d_a_obj_htetu1: Implement gate (USA partial)

--- include/d/actor/d_a_obj_htetu1.h
+++ include/d/actor/d_a_obj_htetu1.h
@@ -2,40 +2,79 @@
 #define D_A_OBJ_HTETU1_H
 
 #include "f_op/f_op_actor.h"
+#include "d/d_particle.h"
+#include "d/d_bg_w.h"
+
+class daObjHtetu1Splash_c {
+public:
+    bool chk_stop() { return !mPlaying; }
+    void delete_s() {
+        if (mSplashCb.getEmitter() != NULL) {
+            mSplashCb.remove();
+            mPlaying = false;
+        }
+    }
+    s16 get_timer() { return mTimer; }
+    void play_particle() {
+        if (mSplashCb.getEmitter() != NULL) {
+            mSplashCb.getEmitter()->playCreateParticle();
+            mPlaying = true;
+        }
+    }
+    void set_pos_y(float y) { mPosition.y = y; }
+    void stop_particle() {
+        if (mSplashCb.getEmitter() != NULL) {
+            mSplashCb.getEmitter()->stopCreateParticle();
+            mPlaying = false;
+        }
+    }
+    void sub_timer() { mTimer--; }
+    void timer_play_particle(short timer) {
+        if (mSplashCb.getEmitter() != NULL) {
+            mSplashCb.getEmitter()->playCreateParticle();
+            mPlaying = true;
+            mTimer = timer;
+        }
+    }
+    void create_s(unsigned short, cXyz*, csXyz*, dKy_tevstr_c*);
+
+    /* 0x00 */ dPa_followEcallBack mSplashCb;
+    /* 0x14 */ cXyz mPosition;
+    /* 0x20 */ csXyz mAngle;
+    /* 0x26 */ s16 mTimer;
+    /* 0x28 */ bool mPlaying;
+};
 
 class daObjHtetu1_c : public fopAc_ac_c {
 public:
-    // TODO: this function is marked as weak in the REL symbol map, but it does not get inlined for some reason?
-    // void check_sw() {}
-    void check_sw();
-
-    void solidHeapCB(fopAc_ac_c*);
-    void create_heap();
+    BOOL check_sw();
+    static BOOL solidHeapCB(fopAc_ac_c*);
+    BOOL create_heap();
     cPhs_State _create();
     bool _delete();
     void init_mtx();
     void unlock();
-    void get_water_h();
+    f32 get_water_h();
     void splash_manager();
     bool _execute();
     bool _draw();
+    static const char M_arcname[];
 
 public:
-    /* Place member variables here */
-};
-
-class daObjHtetu1Splash_c {
-public:
-    void chk_stop() {}
-    void delete_s() {}
-    void get_timer() {}
-    void play_particle() {}
-    void set_pos_y(float) {}
-    void stop_particle() {}
-    void sub_timer() {}
-    void timer_play_particle(short) {}
-
-    void create_s(unsigned short, cXyz*, csXyz*, dKy_tevstr_c*);
+    /* 0x290 */ J3DModel* mpModel;
+    /* 0x294 */ request_of_phase_process_class mPhase;
+    /* 0x29C */ s32 mSwitch;
+    /* 0x2A0 */ cXyz mNextPos;
+    /* 0x2AC */ cXyz mShakeOffset;
+    /* 0x2B8 */ f32 mBottomY;
+    /* 0x2BC */ f32 mShakeAmplitude;
+    /* 0x2C0 */ u16 mShakeTimer;
+    /* 0x2C2 */ u8 mMoveState;
+    /* 0x2C4 */ s16 mEventIdx;
+    /* 0x2C6 */ u8 mEventState;
+    /* 0x2C8 */ s16 mQuakeTimer;
+    /* 0x2CC */ dBgW* mpBgW;
+    /* 0x2D0 */ daObjHtetu1Splash_c mSplash[2];
 };
 
 #endif /* D_A_OBJ_HTETU1_H */
--- src/d/actor/d_a_obj_htetu1.cpp
+++ src/d/actor/d_a_obj_htetu1.cpp
@@ -5,65 +5,233 @@
 
 #include "d/dolzel_rel.h" // IWYU pragma: keep
 #include "d/actor/d_a_obj_htetu1.h"
+#include "d/d_com_inf_game.h"
+#include "d/d_bg_s_func.h"
+#include "d/d_bg_s_wtr_chk.h"
+#include "f_op/f_op_actor_mng.h"
+#include "JSystem/JUtility/JUTAssert.h"
+#include "m_Do/m_Do_ext.h"
+#include "res/Object/Htetu1.h"
+
+static const u16 l_daObjHtetu1_splash_id_table[] = {
+    dPa_name::ID_AK_SN_SIRENWATERGATE00,
+    dPa_name::ID_AK_SN_SIRENWATERGATE01,
+};
+const char daObjHtetu1_c::M_arcname[] = "Htetu1";
 
 /* 00000078-00000178       .text create_s__19daObjHtetu1Splash_cFUsP4cXyzP5csXyzP12dKy_tevstr_c */
-void daObjHtetu1Splash_c::create_s(unsigned short, cXyz*, csXyz*, dKy_tevstr_c*) {
-    /* Nonmatching */
+void daObjHtetu1Splash_c::create_s(unsigned short id, cXyz* pos, csXyz* angle, dKy_tevstr_c* tev) {
+    mPosition = *pos;
+    mAngle = *angle;
+    dComIfGp_particle_set(id, &mPosition, &mAngle, NULL, 255, &mSplashCb);
+    if (mSplashCb.getEmitter() != NULL) mSplashCb.getEmitter()->setGlobalPrmColor(tev->mColorC0.r, tev->mColorC0.g, tev->mColorC0.b);
+    stop_particle();
+    mPlaying = false;
+    mTimer = -2;
 }
 
 /* 00000178-00000198       .text solidHeapCB__13daObjHtetu1_cFP10fopAc_ac_c */
-void daObjHtetu1_c::solidHeapCB(fopAc_ac_c*) {
-    /* Nonmatching */
+BOOL daObjHtetu1_c::solidHeapCB(fopAc_ac_c* actor) {
+    return static_cast<daObjHtetu1_c*>(actor)->create_heap();
 }
 
 /* 00000198-00000298       .text create_heap__13daObjHtetu1_cFv */
-void daObjHtetu1_c::create_heap() {
-    /* Nonmatching */
+BOOL daObjHtetu1_c::create_heap() {
+    // Remaining USA mismatch: register allocation.
+    BOOL result = TRUE;
+    J3DModelData* mdl_data = (J3DModelData*)dComIfG_getObjectRes(M_arcname, dRes_INDEX_HTETU1_BDL_HTETU1_e);
+    JUT_ASSERT(281, mdl_data != 0);
+    if (mdl_data == NULL) {
+        result = FALSE;
+    } else {
+        mpModel = mDoExt_J3DModel__create(mdl_data, 0, 0x11020203);
+        Mtx* matrix = &mpModel->getBaseTRMtx();
+        cBgD_t* bgData = (cBgD_t*)dComIfG_getObjectRes(M_arcname, dRes_INDEX_HTETU1_DZB_HTETU1_e);
+        mpBgW = dBgW_NewSet(bgData, cBgW::MOVE_BG_e, matrix);
+        if (mpBgW == NULL) result = FALSE;
+    }
+    return result;
 }
 
 /* 00000298-00000460       .text _create__13daObjHtetu1_cFv */
 cPhs_State daObjHtetu1_c::_create() {
-    /* Nonmatching */
+    fopAcM_SetupActor(this, daObjHtetu1_c);
+    cPhs_State phase = dComIfG_resLoad(&mPhase, M_arcname);
+    if (phase == cPhs_COMPLEATE_e) {
+        phase = cPhs_ERROR_e;
+        if (fopAcM_entrySolidHeap(this, solidHeapCB, 0xAE0)) {
+            fopAcM_SetMtx(this, mpModel->getBaseTRMtx());
+            mSwitch = fopAcM_GetParam(this) & 0xFF;
+            mBottomY = current.pos.y - 2300.0f;
+            if (check_sw()) {
+                current.pos.y = mBottomY;
+                mEventState = 2;
+            }
+            mQuakeTimer = -1;
+            init_mtx();
+            g_env_light.settingTevStruct(TEV_TYPE_BG1, &current.pos, &tevStr);
+            for (int i = 0; i < 2; i++) mSplash[i].create_s(l_daObjHtetu1_splash_id_table[i], &current.pos, &current.angle, &tevStr);
+            fopAcM_setCullSizeBox(this, -950.0f, -1000.0f, -100.0f, 950.0f, 1300.0f, 100.0f);
+            dComIfG_Bgsp()->Regist(mpBgW, this);
+            mEventIdx = dComIfGp_evmng_getEventIdx("htetu1_open");
+            phase = cPhs_COMPLEATE_e;
+        }
+    }
+    return phase;
 }
 
 /* 00000508-00000610       .text _delete__13daObjHtetu1_cFv */
 bool daObjHtetu1_c::_delete() {
-    /* Nonmatching */
+    if (mQuakeTimer > 0) {
+        dComIfGp_getVibration().StopQuake(-1);
+        mQuakeTimer = -1;
+    }
+    for (int i = 0; i < 2; i++) mSplash[i].delete_s();
+    if (heap != NULL && mpBgW != NULL && mpBgW->ChkUsed()) {
+        dComIfG_Bgsp()->Release(mpBgW);
+        mpBgW = NULL;
+    }
+    dComIfG_resDelete(&mPhase, M_arcname);
+    return true;
 }
 
 /* 00000610-00000648       .text check_sw__13daObjHtetu1_cFv */
-void daObjHtetu1_c::check_sw() {
-    /* Nonmatching */
+inline BOOL daObjHtetu1_c::check_sw() {
+    return dComIfGs_isSwitch(mSwitch, home.roomNo);
 }
 
 /* 00000648-000006E4       .text init_mtx__13daObjHtetu1_cFv */
 void daObjHtetu1_c::init_mtx() {
-    /* Nonmatching */
+    mpModel->setBaseScale(scale);
+    mDoMtx_stack_c::transS(current.pos);
+    mDoMtx_stack_c::ZXYrotM(shape_angle);
+    mpModel->setBaseTRMtx(mDoMtx_stack_c::get());
+    mpModel->calc();
 }
 
 /* 000006E4-000007F8       .text unlock__13daObjHtetu1_cFv */
 void daObjHtetu1_c::unlock() {
-    /* Nonmatching */
+    // Remaining USA mismatch: floating-point result register.
+    cXyz offset = cXyz::BaseY;
+    mNextPos -= mShakeOffset;
+    f32 displacement = mShakeAmplitude;
+    displacement *= cM_ssin(mShakeTimer * 0x859);
+    displacement = (s16)displacement;
+    offset *= std::fabsf(displacement);
+    mNextPos += offset;
+    mShakeOffset = offset;
+    cLib_addCalc(&mShakeAmplitude, 0.0f, 0.13f, 50.0f, 1.0f);
 }
 
 /* 000007F8-0000098C       .text get_water_h__13daObjHtetu1_cFv */
-void daObjHtetu1_c::get_water_h() {
-    /* Nonmatching */
+f32 daObjHtetu1_c::get_water_h() {
+    dBgS_WtrChk chk;
+    cXyz pos = home.pos;
+    f32 height = current.pos.y;
+    cXyz offset;
+    mDoMtx_stack_c::YrotS(current.angle.y);
+    mDoMtx_stack_c::multVec(&cXyz::BaseZ, &offset);
+    offset *= 400.0f;
+    pos += offset;
+    if (dBgS_SplGrpChk_In_ObjGnd(pos, &chk, 1.0f)) height = chk.GetHeight();
+    return height;
 }
 
 /* 00000AB0-00000BCC       .text splash_manager__13daObjHtetu1_cFv */
 void daObjHtetu1_c::splash_manager() {
-    /* Nonmatching */
+    f32 height = get_water_h();
+    for (int i = 0; i < 2; i++) {
+        mSplash[i].set_pos_y(height);
+        s16 timer = mSplash[i].get_timer();
+        if (timer == 0) {
+            if (!mSplash[i].chk_stop()) mSplash[i].stop_particle();
+        } else if (timer > 0 || timer == -1) {
+            if (1400.0f + current.pos.y > height) {
+                if (mSplash[i].chk_stop()) mSplash[i].play_particle();
+            } else {
+                if (!mSplash[i].chk_stop()) mSplash[i].stop_particle();
+            }
+            if (timer > 0) mSplash[i].sub_timer();
+        }
+    }
 }
 
 /* 00000BCC-0000101C       .text _execute__13daObjHtetu1_cFv */
 bool daObjHtetu1_c::_execute() {
-    /* Nonmatching */
+    int i;
+    switch (mEventState) {
+    case 0:
+        if (check_sw()) {
+            if (!eventInfo.checkCommandDemoAccrpt()) {
+                fopAcM_orderOtherEventId(this, mEventIdx);
+                eventInfo.onCondition(2);
+            } else {
+                mShakeAmplitude = 50.0f;
+                mShakeTimer = 70;
+                mShakeOffset = cXyz::BaseY * 50.0f;
+                mDoAud_seStart(JA_SE_READ_RIDDLE_1);
+                mMoveState = 1;
+                mEventState = 1;
+                dComIfGp_getVibration().StartShock(5, -33, cXyz(0.0f, 1.0f, 0.0f));
+                for (i = 0; i < 2; i++) mSplash[i].timer_play_particle(30);
+            }
+        }
+        break;
+    case 1:
+        if (dComIfGp_evmng_endCheck(mEventIdx)) {
+            dComIfGp_event_reset();
+            mEventState = 2;
+        }
+        break;
+    case 2:
+        break;
+    }
+    mNextPos = current.pos;
+    switch (mMoveState) {
+    case 0:
+        break;
+    case 1:
+        unlock();
+        if (mShakeTimer != 0) {
+            mShakeTimer--;
+            mDoAud_seStart(JA_SE_OBJ_ST_KOUSHI_MOVE, &current.pos, 0, dComIfGp_getReverb(current.roomNo));
+        } else {
+            dComIfGp_getVibration().StartQuake(6, 3, cXyz(0.0f, 1.0f, 0.0f));
+            mMoveState = 2;
+            mQuakeTimer = 200;
+            for (i = 0; i < 2; i++) mSplash[i].timer_play_particle(-1);
+        }
+        break;
+    case 2:
+        mNextPos.y -= 5.0f;
+        mDoAud_seStart(JA_SE_OBJ_ST_KOUSHI_MOVE, &current.pos, 0, dComIfGp_getReverb(current.roomNo));
+        if (mNextPos.y <= mBottomY) {
+            for (i = 0; i < 2; i++) mSplash[i].delete_s();
+            mNextPos.y = mBottomY;
+            mMoveState = 0;
+        }
+        break;
+    }
+    current.pos = mNextPos;
+    init_mtx();
+    splash_manager();
+    s16 timer = mQuakeTimer;
+    if (timer == 0) {
+        dComIfGp_getVibration().StopQuake(-1);
+        mQuakeTimer = -1;
+    } else if (timer > 0) {
+        mQuakeTimer--;
+    }
+    if (heap != NULL && mpBgW != NULL && mpBgW->ChkUsed()) mpBgW->Move();
+    return true;
 }
 
 /* 0000101C-0000107C       .text _draw__13daObjHtetu1_cFv */
 bool daObjHtetu1_c::_draw() {
-    /* Nonmatching */
+    g_env_light.settingTevStruct(TEV_TYPE_BG0, &current.pos, &tevStr);
+    g_env_light.setLightTevColorType(mpModel, &tevStr);
+    mDoExt_modelUpdateDL(mpModel);
+    return true;
 }
 
 namespace {
