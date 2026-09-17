71dd7c8d621227ed0db51f60c17248437497e71a
snrubrm <snrubrm@proton.me>
2026-09-17 18:45:49 +0100

    d_a_obj_hami2 OK (USA)

--- include/d/actor/d_a_obj_hami2.h
+++ include/d/actor/d_a_obj_hami2.h
@@ -6,7 +6,8 @@
 namespace daObjHami2 {
     class Act_c : public dBgS_MoveBgActor {
     public:
-        void prm_get_swSave() const {}
+        enum Prm_e { PRM_SWSAVE_W = 8, PRM_SWSAVE_S = 0 };
+        s32 prm_get_swSave() const { return daObj::PrmAbstract(this, PRM_SWSAVE_W, PRM_SWSAVE_S); }
     
         virtual BOOL CreateHeap();
         virtual BOOL Create();
@@ -25,7 +26,17 @@ namespace daObjHami2 {
         virtual BOOL Draw();
     
     public:
-        /* Place member variables here */
+        static const char M_arcname[];
+        static const char M_evname[];
+        static Mtx M_tmp_mtx;
+        /* 0x2C8 */ s16 mGateAngle;
+        /* 0x2CC */ request_of_phase_process_class mPhs;
+        /* 0x2D4 */ J3DModel* mModel;
+        /* 0x2D8 */ dBgW* mStaticBg;
+        /* 0x2DC */ Mtx mStaticMtx;
+        /* 0x30C */ s32 mState;
+        /* 0x310 */ s16 mOpenEvent;
+        /* 0x312 */ s16 mCloseEvent;
     };
 };
 
