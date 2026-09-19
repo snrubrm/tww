d9a8a462518a81cb36c44ab6bf0d48606a79131d
snrubrm <snrubrm@proton.me>
2026-09-19 06:43:41 +0100

    d_a_gnd: Match HIO ctor, helpers, and IsDelete (USA partial)

--- include/d/actor/d_a_gnd.h
+++ include/d/actor/d_a_gnd.h
@@ -2,26 +2,129 @@
 #define D_A_GND_H
 
 #include "f_op/f_op_actor.h"
+#include "d/d_bg_s_acch.h"
+#include "d/d_cc_d.h"
+#include "d/d_particle.h"
+#include "m_Do/m_Do_ext.h"
+#include "m_Do/m_Do_hostIO.h"
+#include "SSystem/SComponent/c_phase.h"
 
 struct gnd_ke_s {
-    
-};
+    /* 0x000 */ cXyz mPos[20];
+    /* 0x0F0 */ cXyz mVel[20];
+};  // Size: 0x1E0
+
+STATIC_ASSERT(sizeof(gnd_ke_s) == 0x1E0);
 
 class gnd_class : public fopEn_enemy_c {
 public:
-    /* 0x02AC */ u8 m02AC[0x13CF - 0x02AC];
+    /* 0x02AC */ request_of_phase_process_class mPhase;
+    /* 0x02B4 */ u8 m2B4[0x02B8 - 0x02B4];
+    /* 0x02B8 */ mDoExt_McaMorf* mpMorf;
+    /* 0x02BC */ mDoExt_btkAnm* mpBtkAnm;
+    /* 0x02C0 */ mDoExt_brkAnm* mpBrkAnm;
+    /* 0x02C4 */ mDoExt_btpAnm* mpBtpAnm;
+    /* 0x02C8 */ u32 mShadowId;
+    /* 0x02CC */ u8 m2CC[0x02D0 - 0x02CC];
+    /* 0x02D0 */ s16 m2D0;
+    /* 0x02D2 */ u8 m2D2[0x030C - 0x02D2];
+    /* 0x030C */ s16 mAction;
+    /* 0x030E */ u8 m30E[0x03A0 - 0x030E];
+    /* 0x03A0 */ s16 mPrmColorR;
+    /* 0x03A2 */ s16 mPrmColorG;
+    /* 0x03A4 */ s16 mPrmColorB;
+    /* 0x03A6 */ u8 m3A6[0x03D8 - 0x03A6];
+    /* 0x03D8 */ s8 m3D8;
+    /* 0x03D9 */ u8 m3D9[0x03E4 - 0x03D9];
+    /* 0x03E4 */ gnd_ke_s mKe[4];
+    /* 0x0B64 */ mDoExt_3DlineMat0_c mLineMat;
+    /* 0x0B80 */ dBgS_AcchCir mAcchCir;
+    /* 0x0BC0 */ dBgS_ObjAcch mAcch;
+    /* 0x0D84 */ dCcD_Stts mStts;
+    /* 0x0DC0 */ dCcD_Cyl mBodyCyl;
+    /* 0x0EF0 */ dCcD_Sph mHeadSph;
+    /* 0x101C */ dCcD_Sph mChestSph;
+    /* 0x1148 */ dCcD_Sph mWeponSph[2];
+    /* 0x13A0 */ cXyz m13A0[2];
+    /* 0x13B8 */ u8 m13B8[0x13CE - 0x13B8];
+    /* 0x13CE */ u8 m13CE;
     /* 0x13CF */ u8 mNextParryOpeningType;
     /* 0x13D0 */ u8 m13D0;
     /* 0x13D1 */ u8 m13D1;
-    /* 0x13D2 */ u8 m13D2[0x15E4 - 0x13D2];
+    /* 0x13D2 */ u8 m13D2[0x13D4 - 0x13D2];
+    /* 0x13D4 */ JPABaseEmitter* mpAttackEff[6];
+    /* 0x13EC */ u8 m13EC[0x14A8 - 0x13EC];
+    /* 0x14A8 */ cXyz m14A8[15];
+    /* 0x155C */ s16 m155C;
+    /* 0x155E */ s16 m155E;
+    /* 0x1560 */ u8 m1560[0x15B0 - 0x1560];
+    /* 0x15B0 */ u8 m15B0;
+    /* 0x15B1 */ u8 m15B1[0x15B4 - 0x15B1];
+    /* 0x15B4 */ dPa_rippleEcallBack mRippleCb;
+    /* 0x15C8 */ cXyz m15C8[2];
+    /* 0x15E0 */ u8 mHioSet;
+    /* 0x15E1 */ u8 m15E1[0x15E4 - 0x15E1];
 };
 
-class daGnd_HIO_c {
+STATIC_ASSERT(sizeof(gnd_class) == 0x15E4);
+
+class daGnd_HIO_c : public JORReflexible {
 public:
     daGnd_HIO_c();
+    virtual ~daGnd_HIO_c() {}
 
 public:
-    /* Place member variables here */
-};
+    /* 0x04 */ s8 mNo;
+    /* 0x05 */ u8 m05;
+    /* 0x06 */ u8 m06;
+    /* 0x07 */ u8 m07;
+    /* 0x08 */ f32 m08;
+    /* 0x0C */ f32 m0C;
+    /* 0x10 */ u8 m10;
+    /* 0x11 */ u8 m11;
+    /* 0x12 */ u8 m12;
+    /* 0x14 */ f32 m14;
+    /* 0x18 */ f32 m18;
+    /* 0x1C */ f32 m1C;
+    /* 0x20 */ f32 m20;
+    /* 0x24 */ f32 m24;
+    /* 0x28 */ f32 m28;
+    /* 0x2C */ f32 m2C;
+    /* 0x30 */ f32 m30;
+    /* 0x34 */ f32 m34;
+    /* 0x38 */ f32 m38;
+    /* 0x3C */ f32 m3C;
+    /* 0x40 */ f32 m40;
+    /* 0x44 */ u8 m44;
+    /* 0x45 */ u8 m45;
+    /* 0x46 */ u8 m46;
+    /* 0x48 */ f32 m48;
+    /* 0x4C */ f32 m4C;
+    /* 0x50 */ f32 m50;
+    /* 0x54 */ f32 m54;
+    /* 0x58 */ f32 m58;
+    /* 0x5C */ f32 m5C;
+    /* 0x60 */ f32 m60;
+    /* 0x64 */ f32 m64;
+    /* 0x68 */ s16 m68;
+    /* 0x6A */ s16 m6A;
+    /* 0x6C */ s16 m6C;
+    /* 0x6E */ s16 m6E;
+    /* 0x70 */ s16 m70;
+    /* 0x72 */ s16 m72;
+    /* 0x74 */ s16 m74;
+    /* 0x76 */ s16 m76;
+    /* 0x78 */ f32 m78;
+    /* 0x7C */ s16 m7C;
+    /* 0x7E */ s16 m7E;
+    /* 0x80 */ s16 m80;
+    /* 0x82 */ s16 m82;
+    /* 0x84 */ s16 m84;
+    /* 0x86 */ s16 m86;
+    /* 0x88 */ u8 m88;
+    /* 0x89 */ u8 m89;
+};  // Size: 0x8C
+
+STATIC_ASSERT(sizeof(daGnd_HIO_c) == 0x8C);
 
 #endif /* D_A_GND_H */
--- src/d/actor/d_a_gnd.cpp
+++ src/d/actor/d_a_gnd.cpp
@@ -6,175 +6,498 @@
 #include "d/dolzel_rel.h" // IWYU pragma: keep
 #include "d/actor/d_a_gnd.h"
 #include "d/d_cc_d.h"
+#include "d/d_com_inf_game.h"
+#include "d/d_demo.h"
+#include "d/d_particle_name.h"
+#include "f_op/f_op_actor_mng.h"
+#include "m_Do/m_Do_audio.h"
+#include "m_Do/m_Do_hostIO.h"
+#include "JSystem/JParticle/JPAEmitter.h"
+#include "res/Object/Gnd.h"
+
+static daGnd_HIO_c l_HIO;
+static bool hio_set;
+
+static u32 attack_eff_joint_d[] = {
+    0x1A, 0x2C, 0x04, 0x07, 0x1A, 0x2C,
+};
+
+static u32 attack_eff_id[] = {
+    dPa_name::ID_AK_SN_GNDAATTACK1HANDSPLASHL00,
+    dPa_name::ID_AK_SN_GNDAATTACK1HANDSPLASHR00,
+    dPa_name::ID_AK_SN_GNDAATTACK1FOOTSPLASHL00,
+    dPa_name::ID_AK_SN_GNDAATTACK1FOOTSPLASHR00,
+    dPa_name::ID_AK_SP_GNDAATTACK1BLURL00,
+    dPa_name::ID_AK_SP_GNDAATTACK1BLURR00,
+    dPa_name::ID_AK_SN_GNDAATTACK2HANDSPLASHL00,
+    dPa_name::ID_AK_SN_GNDAATTACK2HANDSPLASHR00,
+    dPa_name::ID_AK_SN_GNDAATTACK2FOOTSPLASHL00,
+    dPa_name::ID_AK_SN_GNDAATTACK2FOOTSPLASHR00,
+    dPa_name::ID_AK_SP_GNDAATTACK2BLURL00,
+    dPa_name::ID_AK_SP_GNDAATTACK2BLURR00,
+    dPa_name::ID_AK_SN_GNDAATTACK3HANDSPLASHL00,
+    dPa_name::ID_AK_SN_GNDAATTACK3HANDSPLASHR00,
+    dPa_name::ID_AK_SN_GNDAATTACK3FOOTSPLASHL00,
+    dPa_name::ID_AK_SN_GNDAATTACK3FOOTSPLASHR00,
+    dPa_name::ID_AK_SP_GNDAATTACK3BLURL00,
+    dPa_name::ID_AK_SP_GNDAATTACK3BLURR00,
+    dPa_name::ID_AK_SN_GNDBATTACK1HANDSPLASHL00,
+    dPa_name::ID_AK_SN_GNDBATTACK1HANDSPLASHR00,
+    dPa_name::ID_AK_SN_GNDBATTACK1FOOTSPLASHL00,
+    dPa_name::ID_AK_SN_GNDBATTACK1FOOTSPLASHR00,
+    dPa_name::ID_AK_SP_GNDBATTACK1BLURL00,
+    dPa_name::ID_AK_SP_GNDBATTACK1BLURR00,
+    dPa_name::ID_AK_SN_GNDBATTACK2HANDSPLASHL00,
+    dPa_name::ID_AK_SN_GNDBATTACK2HANDSPLASHR00,
+    dPa_name::ID_AK_SN_GNDBATTACK2FOOTSPLASHL00,
+    dPa_name::ID_AK_SN_GNDBATTACK2FOOTSPLASHR00,
+    dPa_name::ID_AK_SP_GNDBATTACK2BLURL00,
+    dPa_name::ID_AK_SP_GNDBATTACK2BLURR00,
+    0x8396, 0x8397, 0x8398, 0x8399,
+    dPa_name::ID_AK_SP_GNDBATTACK3TBLURL00,
+    dPa_name::ID_AK_SP_GNDBATTACK3TBLURR00,
+    0x839A, 0x839B, 0x839C, 0x839D,
+    dPa_name::ID_AK_SP_GNDBATTACK3ABLURL00,
+    dPa_name::ID_AK_SP_GNDBATTACK3ABLURR00,
+    0x839E, 0x839F, 0x83A0, 0x83A1,
+    dPa_name::ID_AK_SP_GNDBATTACK3EBLURL00,
+    dPa_name::ID_AK_SP_GNDBATTACK3EBLURR00,
+    dPa_name::ID_AK_SN_GNDJATTACKTHANDSPLASHL00,
+    dPa_name::ID_AK_SN_GNDJATTACKTHANDSPLASHR00,
+    dPa_name::ID_AK_SN_GNDJATTACKTFOOTSPLASHL00,
+    dPa_name::ID_AK_SN_GNDJATTACKTFOOTSPLASHR00,
+    dPa_name::ID_AK_SP_GNDJATTACKTBLURL00,
+    dPa_name::ID_AK_SP_GNDJATTACKTBLURR00,
+    dPa_name::ID_AK_SN_GNDJATTACKJHANDSPLASHL00,
+    dPa_name::ID_AK_SN_GNDJATTACKJHANDSPLASHR00,
+    dPa_name::ID_AK_SN_GNDJATTACKJFOOTSPLASHL00,
+    dPa_name::ID_AK_SN_GNDJATTACKJFOOTSPLASHR00,
+    dPa_name::ID_AK_SP_GNDJATTACKJBLURL00,
+    dPa_name::ID_AK_SP_GNDJATTACKJBLURR00,
+    dPa_name::ID_AK_SN_GNDJATTACKAHANDSPLASHL00,
+    dPa_name::ID_AK_SN_GNDJATTACKAHANDSPLASHR00,
+    dPa_name::ID_AK_SN_GNDJATTACKAFOOTSPLASHL00,
+    dPa_name::ID_AK_SN_GNDJATTACKAFOOTSPLASHR00,
+    dPa_name::ID_AK_SP_GNDJATTACKABLURL00,
+    dPa_name::ID_AK_SP_GNDJATTACKABLURR00,
+    dPa_name::ID_AK_SN_GNDJATTACKEHANDSPLASHL00,
+    dPa_name::ID_AK_SN_GNDJATTACKEHANDSPLASHR00,
+    dPa_name::ID_AK_SN_GNDJATTACKEFOOTSPLASHL00,
+    dPa_name::ID_AK_SN_GNDJATTACKEFOOTSPLASHR00,
+    dPa_name::ID_AK_SP_GNDJATTACKEBLURL00,
+    dPa_name::ID_AK_SP_GNDJATTACKEBLURR00,
+    dPa_name::ID_AK_SN_GNDNATTACK1HANDSPLASHL00,
+    dPa_name::ID_AK_SN_GNDNATTACK1HANDSPLASHR00,
+    dPa_name::ID_AK_SN_GNDNATTACK1FOOTSPLASHL00,
+    dPa_name::ID_AK_SN_GNDNATTACK1FOOTSPLASHR00,
+    dPa_name::ID_AK_SP_GNDNATTACK1BLURL00,
+    dPa_name::ID_AK_SP_GNDNATTACK1BLURR00,
+    dPa_name::ID_AK_SN_GNDNATTACK2HANDSPLASHL00,
+    dPa_name::ID_AK_SN_GNDNATTACK2HANDSPLASHR00,
+    dPa_name::ID_AK_SN_GNDNATTACK2FOOTSPLASHL00,
+    dPa_name::ID_AK_SN_GNDNATTACK2FOOTSPLASHR00,
+    dPa_name::ID_AK_SP_GNDNATTACK2BLURL00,
+    dPa_name::ID_AK_SP_GNDNATTACK2BLURR00,
+    dPa_name::ID_AK_SN_GNDCOUNTERLHANDSPLASHL00,
+    dPa_name::ID_AK_SN_GNDCOUNTERLHANDSPLASHR00,
+    dPa_name::ID_AK_SN_GNDCOUNTERLFOOTSPLASHL00,
+    dPa_name::ID_AK_SN_GNDCOUNTERLFOOTSPLASHR00,
+    dPa_name::ID_AK_SP_GNDCOUNTERLBLURL00,
+    dPa_name::ID_AK_SP_GNDCOUNTERLBLURR00,
+    dPa_name::ID_AK_SN_GNDCOUNTERRHANDSPLASHL00,
+    dPa_name::ID_AK_SN_GNDCOUNTERRHANDSPLASHR00,
+    dPa_name::ID_AK_SN_GNDCOUNTERRFOOTSPLASHL00,
+    dPa_name::ID_AK_SN_GNDCOUNTERRFOOTSPLASHR00,
+    dPa_name::ID_AK_SP_GNDCOUNTERRBLURL00,
+    dPa_name::ID_AK_SP_GNDCOUNTERRBLURR00,
+    dPa_name::ID_AK_SN_GNDCOUNTERRHANDSPLASHL00,
+    dPa_name::ID_AK_SN_GNDCOUNTERRHANDSPLASHR00,
+    dPa_name::ID_AK_SN_GNDCOUNTERRFOOTSPLASHL00,
+    dPa_name::ID_AK_SN_GNDCOUNTERRFOOTSPLASHR00,
+    dPa_name::ID_AK_SP_GNDCOUNTERRBLURL00,
+    dPa_name::ID_AK_SP_GNDCOUNTERRBLURR00,
+    0, 0, 0, dPa_name::ID_AK_SN_GNDKICKSPLASH00, 0, 0,
+};
 
 /* 000000EC-0000023C       .text __ct__11daGnd_HIO_cFv */
 daGnd_HIO_c::daGnd_HIO_c() {
-    /* Nonmatching */
+    mNo = -1;
+    m05 = 0;
+    m06 = 0;
+    m07 = 1;
+    m08 = 45.0f;
+    m0C = 1.0f;
+    m10 = 0;
+    m11 = 0;
+    m12 = 1;
+    m14 = 5.0f;
+    m18 = 629.0f;
+    m1C = 1200.0f;
+    m20 = 25.0f;
+    m24 = 11.0f;
+    m28 = -0.5f;
+    m2C = 100.0f;
+    m30 = -30.0f;
+    m34 = 20.0f;
+    m38 = -2.0f;
+    m3C = 630.0f;
+    m40 = 400.0f;
+    m44 = 0;
+    m45 = 1;
+    m46 = 1;
+    m48 = 35.0f;
+    m4C = 0.0f;
+    m50 = 5.0f;
+    m54 = 15.0f;
+    m58 = 0.0f;
+    m5C = 30.0f;
+    m60 = -2.0f;
+    m64 = 8.0f;
+    m68 = 30;
+    m6A = 30;
+    m6C = 0;
+    m6E = 15;
+    m70 = 10;
+    m72 = 0;
+    m74 = 10;
+    m76 = 10;
+    m78 = 600.0f;
+    m7C = 10;
+    m7E = 1;
+    m80 = 30;
+    m82 = 3;
+    m84 = 6;
+    m86 = 70;
+    m88 = 0;
+    m89 = 0;
 }
 
 /* 0000023C-000003CC       .text j_demo__FP9gnd_class */
-void j_demo(gnd_class*) {
-    /* Nonmatching */
+static BOOL j_demo(gnd_class* i_this) {
+    if (i_this->demoActorID == 0) {
+        if (i_this->m15B0 != 0) {
+            i_this->m15B0 = 0;
+        }
+    } else {
+        i_this->m15B0 = 1;
+        dDemo_actor_c* demo_actor = dComIfGp_demo_getActor(i_this->demoActorID);
+
+        J3DAnmTexPattern* btp = demo_actor->getP_BtpData("Gnd");
+        if (btp != NULL) {
+            i_this->mpBtpAnm->init(i_this->mpMorf->getModel()->getModelData(), btp, TRUE, J3DFrameCtrl::EMode_NONE, 1.0f, 0, -1, true, 0);
+        }
+
+        J3DAnmTextureSRTKey* btk = demo_actor->getP_BtkData("Gnd");
+        if (btk != NULL) {
+            i_this->mpBtkAnm->init(i_this->mpMorf->getModel()->getModelData(), btk, TRUE, J3DFrameCtrl::EMode_NONE, 1.0f, 0, -1, true, 0);
+        }
+
+        J3DAnmTevRegKey* brk = (J3DAnmTevRegKey*)demo_actor->getP_BrkData("Gnd");
+        if (brk != NULL) {
+            i_this->mpBrkAnm->init(i_this->mpMorf->getModel()->getModelData(), brk, TRUE, J3DFrameCtrl::EMode_LOOP, 1.0f, 0, -1, true, 0);
+        }
+
+        dDemo_setDemoData(i_this, 0x6A, i_this->mpMorf, "Gnd", 0, NULL, 0, 0);
+    }
+    return i_this->m15B0;
 }
 
 /* 000003CC-000003EC       .text checkGround__FP9gnd_classf */
-void checkGround(gnd_class*, float) {
-    /* Nonmatching */
+static BOOL checkGround(gnd_class* i_this, f32 param_2) {
+    return i_this->current.pos.y < 1.0f + param_2;
 }
 
 /* 000003EC-00000510       .text setRipple__FP9gnd_class */
-void setRipple(gnd_class*) {
-    /* Nonmatching */
+static void setRipple(gnd_class* i_this) {
+    if (checkGround(i_this, 0.0f)) {
+        if (i_this->mAcch.ChkGroundHit() &&
+            dComIfG_Bgsp()->GetAttributeCode(i_this->mAcch.m_gnd) == dBgS_Attr_WATER_e &&
+            i_this->mRippleCb.getEmitter() == NULL)
+        {
+            static cXyz scale(1.0f, 1.0f, 1.0f);
+            dComIfGp_particle_setShipTail(
+                dPa_name::ID_AK_JN_HAMON00, &i_this->current.pos, NULL, &scale, 0xFF, &i_this->mRippleCb
+            );
+            if (i_this->mRippleCb.getEmitter() != NULL) {
+                i_this->mRippleCb.setRate(0.0f);
+            }
+        }
+    } else {
+        i_this->mRippleCb.end();
+    }
 }
 
 /* 0000054C-000005DC       .text splash_set__FP9gnd_class */
-void splash_set(gnd_class*) {
-    /* Nonmatching */
+static void splash_set(gnd_class* i_this) {
+    fopAc_ac_c* actor = i_this;
+
+    JPABaseEmitter* emitter = dComIfGp_particle_set(dPa_name::ID_AK_SN_GNDLANDINGSPLASH00, &actor->current.pos);
+    if (emitter != NULL) {
+        emitter->setGlobalPrmColor(i_this->mPrmColorR, i_this->mPrmColorG, i_this->mPrmColorB);
+    }
 }
 
 /* 000005DC-000006B4       .text attack_eff_set__FP9gnd_classi */
-void attack_eff_set(gnd_class*, int) {
-    /* Nonmatching */
+static void attack_eff_set(gnd_class* i_this, int param_2) {
+    fopAc_ac_c* actor = i_this;
+
+    for (int i = 0; i < 6; i++) {
+        u32 id = attack_eff_id[param_2 * 6 + i];
+        if (id == 0) {
+            continue;
+        }
+
+        i_this->mpAttackEff[i] = dComIfGp_particle_set(id, &actor->current.pos);
+        if (i <= 3) {
+            i_this->mpAttackEff[i]->setGlobalPrmColor(i_this->mPrmColorR, i_this->mPrmColorG, i_this->mPrmColorB);
+        }
+    }
 }
 
 /* 000006B4-00000738       .text attack_eff_move__FP9gnd_class */
-void attack_eff_move(gnd_class*) {
-    /* Nonmatching */
+static void attack_eff_move(gnd_class* i_this) {
+    J3DModel* model = i_this->mpMorf->getModel();
+    for (int i = 0; i < 6; i++) {
+        if (i_this->mpAttackEff[i] != NULL) {
+            i_this->mpAttackEff[i]->setGlobalRTMatrix(model->getAnmMtx(attack_eff_joint_d[i]));
+        }
+    }
 }
 
 /* 00000738-0000077C       .text attack_eff_remove__FP9gnd_class */
-void attack_eff_remove(gnd_class*) {
-    /* Nonmatching */
+static void attack_eff_remove(gnd_class* i_this) {
+    for (int i = 0; i < 6; i++) {
+        if (i_this->mpAttackEff[i] != NULL) {
+            i_this->mpAttackEff[i]->becomeInvalidEmitter();
+            i_this->mpAttackEff[i] = NULL;
+        }
+    }
 }
 
 /* 0000077C-00000900       .text anm_init__FP9gnd_classifUcfi */
-void anm_init(gnd_class*, int, float, unsigned char, float, int) {
-    /* Nonmatching */
+static void anm_init(gnd_class* i_this, int bckFileIdx, f32 morf, u8 loopMode, f32 speed, int soundFileIdx) {
+    if (soundFileIdx >= 0) {
+        i_this->mpMorf->setAnm(
+            (J3DAnmTransform*)dComIfG_getObjectRes("Gnd", bckFileIdx),
+            loopMode,
+            morf,
+            speed,
+            0.0f,
+            -1.0f,
+            dComIfG_getObjectRes("Gnd", soundFileIdx)
+        );
+    } else {
+        i_this->mpMorf->setAnm((J3DAnmTransform*)dComIfG_getObjectRes("Gnd", bckFileIdx), loopMode, morf, speed, 0.0f, -1.0f, NULL);
+    }
+
+    attack_eff_remove(i_this);
+
+    int attack_anm[18] = {
+        dRes_INDEX_GND_BCK_AATTACK1_e,
+        dRes_INDEX_GND_BCK_AATTACK2_e,
+        dRes_INDEX_GND_BCK_AATTACK3_e,
+        dRes_INDEX_GND_BCK_BATTACK1_e,
+        dRes_INDEX_GND_BCK_BATTACK2_e,
+        dRes_INDEX_GND_BCK_BATTACK3_TAME_e,
+        dRes_INDEX_GND_BCK_BATTACK3_ATTACK_e,
+        dRes_INDEX_GND_BCK_BATTACK3_END_e,
+        dRes_INDEX_GND_BCK_JATTACK_TAME_e,
+        dRes_INDEX_GND_BCK_JATTACK_JUMP_e,
+        dRes_INDEX_GND_BCK_JATTACK_ATTACK_e,
+        dRes_INDEX_GND_BCK_JATTACK_END_e,
+        dRes_INDEX_GND_BCK_NOM_ATTACK1_e,
+        dRes_INDEX_GND_BCK_NOM_ATTACK2_e,
+        dRes_INDEX_GND_BCK_COUNTERL_e,
+        dRes_INDEX_GND_BCK_COUNTERR_e,
+        dRes_INDEX_GND_BCK_COMBO_GANON_e,
+        dRes_INDEX_GND_BCK_KERI2_e,
+    };
+
+    for (int i = 0; i < 18; i++) {
+        if (bckFileIdx == attack_anm[i]) {
+            attack_eff_set(i_this, i);
+            break;
+        }
+    }
 }
 
 /* 00000900-0000094C       .text z_s_sub__FPvPv */
-void z_s_sub(void*, void*) {
-    /* Nonmatching */
+static void* z_s_sub(void* param_1, void*) {
+    if (fopAcM_IsActor(param_1) && fopAcM_GetName(param_1) == fpcNm_PZ_e) {
+        return param_1;
+    }
+    return NULL;
 }
 
 /* 0000094C-00000C38       .text daGnd_Draw__FP9gnd_class */
 static BOOL daGnd_Draw(gnd_class*) {
     /* Nonmatching */
+    return TRUE;
 }
 
 /* 00000C38-00000C6C       .text player_view_check__FP9gnd_classs */
-void player_view_check(gnd_class*, short) {
-    /* Nonmatching */
+static BOOL player_view_check(gnd_class* i_this, s16 param_2) {
+    s16 diff = i_this->shape_angle.y - param_2;
+    if (diff < 0) {
+        diff = -diff;
+    }
+    if ((u16)diff < 0x4000) {
+        return TRUE;
+    }
+    return FALSE;
 }
 
 /* 00000C6C-00000F24       .text ke_control__FP9gnd_classP8gnd_ke_sf */
-void ke_control(gnd_class*, gnd_ke_s*, float) {
+static void ke_control(gnd_class*, gnd_ke_s*, f32) {
     /* Nonmatching */
 }
 
 /* 00000F24-00000F68       .text ke_pos_set__FP9gnd_classP8gnd_ke_si */
-void ke_pos_set(gnd_class*, gnd_ke_s*, int) {
-    /* Nonmatching */
+static void ke_pos_set(gnd_class* i_this, gnd_ke_s* param_2, int param_3) {
+    cXyz* pos = i_this->mLineMat.getPos(param_3);
+    for (s32 i = 0; i < 20; pos++, i++) {
+        *pos = param_2->mPos[i];
+    }
 }
 
 /* 00000F68-00001140       .text ke_move__FP9gnd_class */
-void ke_move(gnd_class*) {
+static void ke_move(gnd_class*) {
     /* Nonmatching */
 }
 
 /* 00001140-00001278       .text pos_move__FP9gnd_classSc */
-void pos_move(gnd_class*, signed char) {
+static void pos_move(gnd_class*, s8) {
     /* Nonmatching */
 }
 
 /* 00001278-00001334       .text wait_set__FP9gnd_class */
-void wait_set(gnd_class*) {
-    /* Nonmatching */
+static void wait_set(gnd_class* i_this) {
+    fopAc_ac_c* actor = i_this;
+
+    if (i_this->m3D8 <= 1) {
+        anm_init(i_this, dRes_INDEX_GND_BCK_WAIT_e, 10.0f, J3DFrameCtrl::EMode_LOOP, 1.0f, -1);
+        fopAcM_monsSeStart(actor, JA_SE_CV_GN_LAUGH_WAIT, 0);
+    } else {
+        anm_init(i_this, dRes_INDEX_GND_BCK_WAIT2_e, 10.0f, J3DFrameCtrl::EMode_LOOP, 1.0f, -1);
+    }
 }
 
 /* 00001334-00001974       .text move0__FP9gnd_class */
-void move0(gnd_class*) {
+static void move0(gnd_class*) {
     /* Nonmatching */
 }
 
 /* 00001974-000028D0       .text attack0__FP9gnd_class */
-void attack0(gnd_class*) {
+static void attack0(gnd_class*) {
     /* Nonmatching */
 }
 
 /* 000028D0-00002B68       .text attack1__FP9gnd_class */
-void attack1(gnd_class*) {
+static void attack1(gnd_class*) {
     /* Nonmatching */
 }
 
 /* 00002B68-00002D7C       .text attack2__FP9gnd_class */
-void attack2(gnd_class*) {
+static void attack2(gnd_class*) {
     /* Nonmatching */
 }
 
 /* 00002D7C-00003688       .text attackPZ__FP9gnd_class */
-void attackPZ(gnd_class*) {
+static void attackPZ(gnd_class*) {
     /* Nonmatching */
 }
 
 /* 00003688-000038B0       .text attack_last__FP9gnd_class */
-void attack_last(gnd_class*) {
+static void attack_last(gnd_class*) {
     /* Nonmatching */
 }
 
 /* 000038B0-000042D0       .text defence0__FP9gnd_class */
-void defence0(gnd_class*) {
+static void defence0(gnd_class*) {
     /* Nonmatching */
 }
 
 /* 000042D0-00004330       .text finish__FP9gnd_class */
-void finish(gnd_class*) {
-    /* Nonmatching */
+static void finish(gnd_class* i_this) {
+    i_this->mAction = 5;
+    i_this->m13CE = 0;
+    switch (i_this->m2D0) {
+    case 0:
+        i_this->m155E = 100;
+        i_this->m2D0 = 1;
+        break;
+    case 1:
+        i_this->m2D0 = 2;
+        break;
+    case 2:
+        i_this->m2D0 = 3;
+        break;
+    }
 }
 
 /* 00004330-00004760       .text damage__FP9gnd_class */
-void damage(gnd_class*) {
+static void damage(gnd_class*) {
     /* Nonmatching */
 }
 
 /* 00004760-0000501C       .text damage_check__FP9gnd_class */
-void damage_check(gnd_class*) {
+static void damage_check(gnd_class*) {
     /* Nonmatching */
 }
 
 /* 0000501C-0000509C       .text shot_s_sub__FPvPv */
-void shot_s_sub(void*, void*) {
-    /* Nonmatching */
+static void* shot_s_sub(void* param_1, void*) {
+    if (fopAcM_IsActor(param_1) && fopAcM_GetName(param_1) == fpcNm_HIMO2_e) {
+        goto check_speed;
+    }
+    s16 name = fopAcM_GetName(param_1);
+    if (name == fpcNm_BOOMERANG_e) {
+        goto check_speed;
+    }
+    if (name == fpcNm_HOOKSHOT_e) {
+        goto check_speed;
+    }
+    if (name != fpcNm_ARROW_e) {
+        return NULL;
+    }
+check_speed:
+    if (((fopAc_ac_c*)param_1)->speedF >= 10.0f) {
+        return param_1;
+    }
+    return NULL;
 }
 
 /* 0000509C-000050B0       .text demowait__FP9gnd_class */
-void demowait(gnd_class*) {
-    /* Nonmatching */
+static void demowait(gnd_class* i_this) {
+    i_this->mAction = 5;
+    i_this->m13CE = 0;
 }
 
 /* 000050B0-000050C8       .text yawait__FP9gnd_class */
-void yawait(gnd_class*) {
-    /* Nonmatching */
+static void yawait(gnd_class* i_this) {
+    i_this->current.pos.y = 0.0f;
+    i_this->m13CE = 0;
 }
 
 /* 000050C8-000054F0       .text gnd_move__FP9gnd_class */
-void gnd_move(gnd_class*) {
+static void gnd_move(gnd_class*) {
     /* Nonmatching */
 }
 
 /* 000054F0-00006B04       .text demo_camera__FP9gnd_class */
-void demo_camera(gnd_class*) {
+static void demo_camera(gnd_class*) {
     /* Nonmatching */
 }
 
 /* 00006B04-00006D0C       .text body_flash__FP9gnd_class */
-void body_flash(gnd_class*) {
+static void body_flash(gnd_class*) {
     /* Nonmatching */
 }
 
 /* 00006D0C-000076B8       .text daGnd_Execute__FP9gnd_class */
 static BOOL daGnd_Execute(gnd_class*) {
     /* Nonmatching */
+    return TRUE;
 }
 
 /* 000076B8-000076C0       .text daGnd_IsDelete__FP9gnd_class */
@@ -183,13 +506,20 @@ static BOOL daGnd_IsDelete(gnd_class*) {
 }
 
 /* 000076C0-00007738       .text daGnd_Delete__FP9gnd_class */
-static BOOL daGnd_Delete(gnd_class*) {
-    /* Nonmatching */
+static BOOL daGnd_Delete(gnd_class* i_this) {
+    dComIfG_resDelete(&i_this->mPhase, "Gnd");
+    i_this->mRippleCb.end();
+    if (i_this->mHioSet != 0) {
+        hio_set = false;
+        mDoHIO_deleteChild(l_HIO.mNo);
+    }
+    return TRUE;
 }
 
 /* 00007738-00007A78       .text useHeapInit__FP10fopAc_ac_c */
 static BOOL useHeapInit(fopAc_ac_c*) {
     /* Nonmatching */
+    return TRUE;
 }
 
 /* 00007AC0-00007CFC       .text daGnd_Create__FP10fopAc_ac_c */
@@ -308,6 +638,7 @@ static cPhs_State daGnd_Create(fopAc_ac_c*) {
             /* Radius */ 70.0f,
         }},
     };
+    return cPhs_ERROR_e;
 }
 
 static actor_method_class l_daGnd_Method = {
