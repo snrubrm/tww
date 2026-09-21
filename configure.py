36ca6c564caf8d815caf4b17970ca50c3c02ba25
snrubrm <snrubrm@proton.me>
2026-09-21 18:31:53 +0100

    configure: Mark newly matched units as matching for USA

--- configure.py
+++ configure.py
@@ -1618,7 +1618,7 @@ config.libs = [
     ActorRel(Matching,    "d_a_bdk"),
     ActorRel(Matching,    "d_a_bdkobj"),
     ActorRel(Matching,    "d_a_bgn"),
-    ActorRel(MatchingFor("D44J01"), "d_a_bgn2"),
+    ActorRel(MatchingFor("D44J01", "GZLE01"), "d_a_bgn2"),
     ActorRel(Matching,    "d_a_bgn3"),
     ActorRel(MatchingFor("GZLE01"), "d_a_bigelf"),
     ActorRel(MatchingFor("GZLJ01", "GZLE01", "GZLP01"),    "d_a_bk"),
