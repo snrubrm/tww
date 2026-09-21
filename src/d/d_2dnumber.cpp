05c99146f14072c1495d9e6c31006ae250258dec
snrubrm <snrubrm@proton.me>
2026-09-21 09:31:59 +0100

    d_2dnumber: Match leftover messageSet (USA partial)

--- src/d/d_2dnumber.cpp
+++ src/d/d_2dnumber.cpp
@@ -415,7 +415,6 @@ f32 dDlst_2DOutFont_c::iconset(int i_iconNo, char** param_1) {
 }
 
 /* 800C9D5C-800CA8A8       .text messageSet__17dDlst_2DOutFont_cFUl */
-// NONMATCHING - leftover GPR/FPR alloc (loop char r5 vs r3, y-offset f31 vs f29)
 void dDlst_2DOutFont_c::messageSet(u32 i_msgNo) {
     fopMsgM_itemMsgGet_c msgGet;
     mesg_header* head_p = msgGet.getMesgHeader(i_msgNo);
@@ -531,7 +530,7 @@ void dDlst_2DOutFont_c::messageSet(u32 i_msgNo) {
 #if VERSION < VERSION_PAL
             int hi_nibble = ((u8)*message >> 4) & 0xF;
             if (hi_nibble == 8 || hi_nibble == 9) {
-                u8 t0 = message[0];
+                u8 t0 = *(u8*)message;
                 s8 temp_r4_2 = t0;
                 *(dst++) = t0;
 
@@ -567,7 +566,7 @@ void dDlst_2DOutFont_c::messageSet(u32 i_msgNo) {
             } else
 #endif
             {
-                u8 t = *message;
+                u8 t = *(u8*)message;
                 s8 var_r5_2 = t;
                 *(dst++) = t;
                 message++;
@@ -613,12 +612,12 @@ void dDlst_2DOutFont_c::messageSet(u32 i_msgNo) {
     *dst = 0;
 
     #if VERSION >= VERSION_USA
-    var_f29 = 0.0f;
+    f32 y = 0.0f;
     if (m74 == 0) {
-        var_f29 = ((mpTextBox->getHeight() - m6C) - m82 * mpTextBox->getLineSpace()) / 2;
+        y = ((mpTextBox->getHeight() - m6C) - m82 * mpTextBox->getLineSpace()) / 2;
     }
     #else
-    var_f29 = ((mpTextBox->getHeight() - m6C) - m82 * mpTextBox->getLineSpace()) / 2;
+    f32 y = ((mpTextBox->getHeight() - m6C) - m82 * mpTextBox->getLineSpace()) / 2;
     #endif
 
     char sp14[16];
@@ -632,7 +631,7 @@ void dDlst_2DOutFont_c::messageSet(u32 i_msgNo) {
             }
 
             if (m76[i] != -1) {
-                m44[i] += var_f29 + (m76[i] * mpTextBox->getLineSpace());
+                m44[i] += y + (m76[i] * mpTextBox->getLineSpace());
             }
         }
     } else {
@@ -641,7 +640,7 @@ void dDlst_2DOutFont_c::messageSet(u32 i_msgNo) {
 
     strcat(m88, sp14);
     strcat(m88, sp104);
-    mpTextBox->shiftSet(0.0f, var_f29);
+    mpTextBox->shiftSet(0.0f, y);
 
     if (m28 == NULL) {
         mpTextBox->setString(m88);
@@ -650,7 +649,7 @@ void dDlst_2DOutFont_c::messageSet(u32 i_msgNo) {
     }
 
     if (mpRubyTextBox != NULL) {
-        mpRubyTextBox->shiftSet(0.0f, var_f29);
+        mpRubyTextBox->shiftSet(0.0f, y);
         strcat(mEC, sp14);
         strcat(mEC, spA0);
 
