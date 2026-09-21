96ce398641165d307470f641d45d9b82736888fb
snrubrm <snrubrm@proton.me>
2026-09-21 16:35:06 +0100

    configure: Mark newly matched units as matching for USA

--- configure.py
+++ configure.py
@@ -652,7 +652,7 @@ config.libs = [
             Object(NonMatching, "d/d_message.cpp"),
             Object(NonMatching, "d/d_message_paper.cpp"),
             Object(NonMatching, "d/d_meter.cpp"),
-            Object(NonMatching, "d/d_minigame_starter.cpp"),
+            Object(MatchingFor("GZLE01"), "d/d_minigame_starter.cpp"),
             Object(MatchingFor("GZLE01"), "d/d_minigame_terminater.cpp"),
             Object(MatchingFor("GZLE01"), "d/d_msg.cpp"),
             Object(Matching,    "d/d_name.cpp"),
