1a927dd1ec643161f8803a38fcc652b44f4c5a92
snrubrm <snrubrm@proton.me>
2026-09-21 19:41:16 +0100

    configure: Mark newly matched units as matching for USA

--- configure.py
+++ configure.py
@@ -897,7 +897,7 @@ config.libs = [
             Object(Matching,    "JSystem/JAudio/osdsp.c", extra_cflags=["-lang c++", "-O4", "-func_align 32", "-str nopool"]),
             Object(Matching,    "JSystem/JAudio/osdsp_task.c", extra_cflags=["-lang c++", "-O4", "-func_align 32"]),
             Object(Matching,    "JSystem/JAudio/JAIAnimation.cpp"),
-            Object(NonMatching, "JSystem/JAudio/JAIBasic.cpp"),
+            Object(MatchingFor("GZLE01"), "JSystem/JAudio/JAIBasic.cpp"),
             Object(Matching,    "JSystem/JAudio/JAIBankWave.cpp"),
             Object(Matching,    "JSystem/JAudio/JAIConst.cpp"),
             Object(Matching,    "JSystem/JAudio/JAIDummyObject.cpp"),
