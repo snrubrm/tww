b369545d54db102096d7893ed86008667601fad3
snrubrm <snrubrm@proton.me>
2026-09-21 10:45:16 +0100

    d_a_obj_buoyflag: Match leftover draw_hasi (USA partial)

--- src/d/actor/d_a_obj_buoyflag.cpp
+++ src/d/actor/d_a_obj_buoyflag.cpp
@@ -272,7 +272,6 @@ void daObjBuoyflag::Packet_c::draw_hata(Act_c* actor) {
 
 /* 00000744-00000C4C       .text draw_hasi__Q213daObjBuoyflag8Packet_cFPQ213daObjBuoyflag5Act_c */
 void daObjBuoyflag::Packet_c::draw_hasi(Act_c* actor) {
-    // USA: texture setup instruction scheduling differs.
     dKy_tevstr_c* tev = &actor->tevStr;
     j3dSys.reinitGX();
     GXSetNumIndStages(0);
@@ -293,7 +292,8 @@ void daObjBuoyflag::Packet_c::draw_hasi(Act_c* actor) {
     GXInitTexObjLOD(&texObj, GX_LINEAR, GX_LINEAR, 0.0f, 0.0f, 0.0f, GX_FALSE, GX_FALSE, GX_ANISO_1);
     GXLoadTexObj(&texObj, GX_TEXMAP0);
     ResTIMG* texInfo = (ResTIMG*)dComIfG_getObjectRes(L_arcname, dRes_INDEX_CLOTH_BTI_CLOTHTOON_e);
-    void* image = (u8*)texInfo + texInfo->imageOffset;
+    u8* image = (u8*)texInfo;
+    image += texInfo->imageOffset;
     GXInitTexObj(&texObj, image, texInfo->width, texInfo->height,
         GXTexFmt(texInfo->format), GXTexWrapMode(texInfo->wrapS), GXTexWrapMode(texInfo->wrapT), GXBool(texInfo->mipmapCount > 1));
     GXInitTexObjLOD(&texObj, GXTexFilter(texInfo->minFilter), GXTexFilter(texInfo->magFilter),
