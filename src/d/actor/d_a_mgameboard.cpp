d99c6ecf47b7024cbd93395511d3c7d2b119d755
snrubrm <snrubrm@proton.me>
2026-09-21 09:51:41 +0100

    d_a_mgameboard: Match leftover MinigameMain and __sinit (USA partial)

--- src/d/actor/d_a_mgameboard.cpp
+++ src/d/actor/d_a_mgameboard.cpp
@@ -353,6 +353,16 @@ void daMgBoard_c::set_mtx() {
     }
 }
 
+static cXyz dummy_func() {
+    // There was an unused function here that got stripped out, but it still affected literal and weak function ordering.
+    f32* dummy = NULL;
+    *dummy = 0.8f;
+    *dummy = 0.75f;
+    *dummy = -0.75f;
+    cXyz temp(0.0f, 0.0f, 0.0f);
+    return temp;
+}
+
 /* 00000E28-00000FD8       .text _execute__11daMgBoard_cFv */
 bool daMgBoard_c::_execute() {
     u8 highScore = dComIfGs_getEventReg(0xBEFF);
