e1453b5889377eadd3fced394dce74fe241c93ee
snrubrm <snrubrm@proton.me>
2026-09-21 18:31:11 +0100

    d_a_bgn2: Match leftover attack_eff_set (USA partial)

--- include/JSystem/JParticle/JPAEmitter.h
+++ include/JSystem/JParticle/JPAEmitter.h
@@ -212,7 +212,7 @@ public:
 
     u8 getGlobalAlpha() { return mGlobalPrmColor.a; }
     void setGlobalAlpha(u8 alpha) { mGlobalPrmColor.a = alpha; }
-    void setGlobalRTMatrix(MtxP mtx) {
+    void setGlobalRTMatrix(const MtxP mtx) {
         JPASetRMtxTVecfromMtx(mtx, mGlobalRotation, mGlobalTranslation);
     }
     void setGlobalSRTMatrix(MtxP mtx) {
