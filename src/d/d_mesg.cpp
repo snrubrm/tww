90196b94099678541171946491a89be115ce0033
snrubrm <snrubrm@proton.me>
2026-09-21 10:25:40 +0100

    d_mesg: Match leftover dotAnime (USA partial)

--- src/d/d_mesg.cpp
+++ src/d/d_mesg.cpp
@@ -1265,9 +1265,10 @@ void dMesg_screenData_c::dotAnimeInit() {
     resetTimer();
 }
 
+static inline int dMesg_dotReverseTimer(int timer) { return 60 - timer; }
+
 /* 801E3194-801E36A8       .text dotAnime__18dMesg_screenData_cFv */
 void dMesg_screenData_c::dotAnime() {
-    /* Nonmatching */
     f32 f31 = 150.0f - field_0x1b0.r;
     f32 f30 = 150.0f - field_0x1b0.g;
     f32 f29 = 150.0f - field_0x1b0.b;
@@ -1292,7 +1293,7 @@ void dMesg_screenData_c::dotAnime() {
             fopMsgM_setInitAlpha(&field_0x18);
             resetTimer();
         } else if (mTimer > 30) {
-            f32 tmp = fopMsgM_valueIncrease(30, 60 - mTimer, 0);
+            f32 tmp = fopMsgM_valueIncrease(30, dMesg_dotReverseTimer(mTimer), 0);
             black.r = field_0x1b0.r + f31 * tmp;
             black.g = field_0x1b0.g + f30 * tmp;
             black.b = field_0x1b0.b + f29 * tmp;
