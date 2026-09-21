e71403bf38686ca4f0c92de03d121c83c361e378
snrubrm <snrubrm@proton.me>
2026-09-21 12:55:10 +0100

    d_message_paper: Match leftover dMsg3_textPosition (USA partial)

--- src/d/d_message_paper.cpp
+++ src/d/d_message_paper.cpp
@@ -433,10 +433,14 @@ void dMsg3_yose_select(sub_msg3_class* i_Msg, u8 i_index) {
     dMsg3_textPosition(i_Msg, i_index);
 }
 
+inline int dMsg3_getShiftY(sub_msg3_class* i_Msg, u8 i_index) {
+    return i_Msg->field_0xeac * (2 - i_Msg->field_0xec8[i_index]);
+}
+
 /* 801EC8CC-801EC97C       .text dMsg3_textPosition__FP14sub_msg3_classUc */
 void dMsg3_textPosition(sub_msg3_class* i_Msg, u8 i_index) {
     f32 r7 = 0.0f;
-    int temp_r0 = i_Msg->field_0xeac * (2 - i_Msg->field_0xec8[i_index]);
+    int temp_r0 = dMsg3_getShiftY(i_Msg, i_index);
     ((J2DTextBox*)i_Msg->text_pane[i_index].pane)->shiftSet(r7, temp_r0);
     ((J2DTextBox*)i_Msg->ruby_pane[i_index].pane)->shiftSet(r7, temp_r0);
     ((J2DTextBox*)i_Msg->textSdw_pane[i_index].pane)->shiftSet(r7, temp_r0);
