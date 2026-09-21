cd2c384382974238a9edc88ccf836449497902ee
snrubrm <snrubrm@proton.me>
2026-09-21 12:20:42 +0100

    configure: Mark newly matched units as matching for USA

--- configure.py
+++ configure.py
@@ -643,7 +643,7 @@ config.libs = [
             Object(NonMatching, "d/d_menu_collect.cpp"),
             Object(MatchingFor("GZLJ01", "GZLE01", "GZLP01"), "d/d_menu_dmap.cpp"),
             Object(MatchingFor("GZLJ01", "GZLE01", "GZLP01"), "d/d_menu_fmap.cpp"),
-            Object(NonMatching, "d/d_menu_fmap2.cpp"),
+            Object(MatchingFor("GZLE01"), "d/d_menu_fmap2.cpp"),
             Object(MatchingFor("GZLE01"), "d/d_menu_item.cpp"),
             Object(Matching,    "d/d_menu_option.cpp"),
             Object(Matching,    "d/d_menu_save.cpp"),
