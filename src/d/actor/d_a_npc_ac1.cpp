1f13a407eb2846a4235e110b3a97702c0276e8bf
snrubrm <snrubrm@proton.me>
2026-09-22 12:07:18 +0100

    d_a_npc_ac1: Match leftover demo (demo partial)

--- src/d/actor/d_a_npc_ac1.cpp
+++ src/d/actor/d_a_npc_ac1.cpp
@@ -526,8 +526,12 @@ u8 daNpc_Ac1_c::demo() {
         dDemo_actor_c* actor = dComIfGp_demo_getActor(demoActorID);
         if (m_hed_tex_pttrn != NULL) {
             mBtpFrame++;
+#if VERSION == VERSION_DEMO
+            if (mBtpFrame >= m_hed_tex_pttrn->getFrameMax()) mBtpFrame = m_hed_tex_pttrn->getFrameMax();
+#else
             int end = m_hed_tex_pttrn->getFrameMax();
             if (mBtpFrame >= (s16)end) mBtpFrame = end;
+#endif
         }
         J3DAnmTexPattern* btp = actor->getP_BtpData("Ac");
         if (btp != NULL) {
