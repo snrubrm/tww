601bc02d21a5e31f6c6ed163724b95cecc412c2a
snrubrm <snrubrm@proton.me>
2026-09-22 12:07:19 +0100

    d_a_npc_btsw: Match leftover Wait (demo partial)

--- src/d/actor/d_a_npc_btsw.cpp
+++ src/d/actor/d_a_npc_btsw.cpp
@@ -1293,11 +1293,19 @@ void SwMail2_c::Wait() {
     }
 
     field_0x24 = *field_0x5C;
+#if VERSION == VERSION_DEMO
+    diff = diff * (120.0f + REG10_F(0));
+    field_0x24 += diff;
+
+    field_0x24.y -= 40.0f + REG10_F(1);
+    field_0x48.set((s16)field_0x4E.x, field_0x4E.y + new_y + REG10_S(1), field_0x4E.z + REG10_S(2));
+#else
     diff = diff * 120.0f;
     field_0x24 += diff;
 
     field_0x24.y -= 40.0f;
     field_0x48.set(field_0x4E.x, field_0x4E.y + new_y, field_0x4E.z);
+#endif
     set_mtx();
 }
 
