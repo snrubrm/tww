35a5c1a1039167aa2cb7f1c09c2d9300574d0961
snrubrm <snrubrm@proton.me>
2026-09-22 11:28:31 +0100

    d_a_sitem: Match leftover demo functions, improve control1 and hand_move (demo partial)

--- src/d/actor/d_a_sitem.cpp
+++ src/d/actor/d_a_sitem.cpp
@@ -20,11 +20,13 @@ static void hand_draw(sitem_class* i_this) {
         mDoExt_modelUpdateDL(model);
     }
     GXColor color = {0, 180, 50, 255};
-    i_this->mLine.update(10, color, &i_this->tevStr);
+    GXColor& c = color;
+    i_this->mLine.update(10, c, &i_this->tevStr);
     dComIfGd_set3DlineMat(&i_this->mLine);
     if (i_this->mCutLength > 0.1f) {
         GXColor color = {0, 180, 50, 255};
-        i_this->mCutLine.update(5, color, &i_this->tevStr);
+        GXColor& c = color;
+        i_this->mCutLine.update(5, c, &i_this->tevStr);
         dComIfGd_set3DlineMat(&i_this->mCutLine);
     }
 }
@@ -39,13 +41,14 @@ static BOOL daSitem_Draw(sitem_class* i_this) {
 /* 00000248-0000034C       .text hand_mtx_set__FP11sitem_class */
 static void hand_mtx_set(sitem_class* i_this) {
     MtxTrans(i_this->mModelPos.x, i_this->mModelPos.y, i_this->mModelPos.z, false);
-    mDoMtx_XrotM(*calc_mtx, i_this->mModelAngle.x);
-    mDoMtx_YrotM(*calc_mtx, i_this->mModelAngle.y);
-    mDoMtx_XrotM(*calc_mtx, REG12_S(1) + 0x4000);
+    cMtx_XrotM(*calc_mtx, i_this->mModelAngle.x);
+    cMtx_YrotM(*calc_mtx, i_this->mModelAngle.y);
+    cMtx_XrotM(*calc_mtx, REG12_S(1) + 0x4000);
     cXyz offset(0.0f, -60.0f + REG12_F(4), 0.0f);
     MtxPosition(&offset, &i_this->mCollisionPos);
     MtxTrans(0.0f, -50.0f + REG12_F(3), 0.0f, true);
-    i_this->mpModel->setBaseTRMtx(*calc_mtx);
+    J3DModel* model = i_this->mpModel;
+    model->setBaseTRMtx(*calc_mtx);
 }
 
 /* 00000388-00000410       .text control3__FP11sitem_class */
@@ -65,20 +68,20 @@ static void control1(sitem_class* i_this) {
     cXyz offset, wave, extension, worldWave;
     i_this->mSegments[0].pos = i_this->current.pos;
     sitem_s* segment = &i_this->mSegments[1];
-    mDoMtx_YrotS(*calc_mtx, i_this->current.angle.y);
-    mDoMtx_XrotM(*calc_mtx, i_this->current.angle.x);
+    cMtx_YrotS(*calc_mtx, i_this->current.angle.y);
+    cMtx_XrotM(*calc_mtx, i_this->current.angle.x);
     offset.x = 0.0f;
     offset.y = 0.0f;
     offset.z = i_this->mExtension;
     MtxPosition(&offset, &extension);
     offset.z = i_this->mSegmentLength;
     f32 amplitude = i_this->mWave;
-    f32 influence = 1.0f;
     for (i = 1; i < 9; i++, segment++) {
         wave.x = amplitude * cM_ssin(i_this->mFrame * (REG0_S(5) + 1100) + i * (REG0_S(6) + 4000));
         wave.y = g_d[i];
         wave.z = amplitude * cM_scos(i_this->mFrame * (REG0_S(7) + 800) + i * (REG0_S(8) + 4000));
         MtxPosition(&wave, &worldWave);
+        f32 influence = 1.0f;
         x = worldWave.x * influence + (segment->pos.x - segment[-1].pos.x + extension.x * influence);
         y = worldWave.y * influence + (segment->pos.y - segment[-1].pos.y + extension.y * influence);
         z = worldWave.z * influence + (segment->pos.z - segment[-1].pos.z + extension.z * influence);
@@ -130,8 +133,8 @@ static void cut_control1(sitem_class* i_this) {
     cXyz offset, wave, worldWave;
     i_this->mCutSegments[0].pos = i_this->current.pos;
     sitem_s* segment = &i_this->mCutSegments[1];
-    mDoMtx_YrotS(*calc_mtx, i_this->current.angle.y);
-    mDoMtx_XrotM(*calc_mtx, i_this->current.angle.x);
+    cMtx_YrotS(*calc_mtx, i_this->current.angle.y);
+    cMtx_XrotM(*calc_mtx, i_this->current.angle.x);
     offset.z = i_this->mCutLength;
     f32 amplitude = 50.0f + REG0_F(18);
     for (i = 1; i < 5; i++, segment++) {
@@ -151,6 +154,21 @@ static void cut_control1(sitem_class* i_this) {
         MtxPull();
         segment->pos = segment[-1].pos + wave;
     }
+#if VERSION == VERSION_DEMO
+    if (i_this->mpSmokeEmitter[0] == NULL && i_this->mSmokeActive) {
+        i_this->mSmoke[0].remove();
+        i_this->mpSmokeEmitter[0] = dComIfGp_particle_set(0x8184, &i_this->mCutSegments[4].pos, &i_this->mSmokeAngle[0], NULL, 255, &i_this->mSmoke[0], fopAcM_GetRoomNo(i_this));
+        i_this->mHitTimer = 60;
+    } else {
+        offset = i_this->mCutSegments[4].pos - i_this->mCutSegments[3].pos;
+        i_this->mSmokeAngle[0].y = cM_atan2s(offset.x, offset.z);
+        i_this->mSmokeAngle[0].x = -cM_atan2s(offset.y, std::sqrtf(offset.x * offset.x + offset.z * offset.z));
+        if (i_this->mHitTimer == 1 && i_this->mpSmokeEmitter[0]) {
+            i_this->mpSmokeEmitter[0]->becomeInvalidEmitter();
+            i_this->mpSmokeEmitter[0] = NULL;
+        }
+    }
+#else
     if (i_this->mSmoke[0].getEmitter() == NULL && i_this->mSmokeActive) {
         i_this->mSmoke[0].remove();
         dComIfGp_particle_set(0x8184, &i_this->mCutSegments[4].pos, &i_this->mSmokeAngle[0], NULL, 255, &i_this->mSmoke[0], fopAcM_GetRoomNo(i_this));
@@ -161,6 +179,7 @@ static void cut_control1(sitem_class* i_this) {
         i_this->mSmokeAngle[0].x = -cM_atan2s(offset.y, std::sqrtf(offset.x * offset.x + offset.z * offset.z));
         if (i_this->mHitTimer == 1 && i_this->mSmoke[0].getEmitter()) i_this->mSmoke[0].remove();
     }
+#endif
 }
 
 /* 00000E2C-00001058       .text my_break__FP11sitem_class */
@@ -168,14 +187,16 @@ static void my_break(sitem_class* i_this) {
     i_this->mTimers[0] = 50;
     i_this->mMode = 6;
     csXyz angle(0, i_this->current.angle.y, 0);
-    fopAcM_createItemFromTable(&i_this->mPos, i_this->mItemTable, i_this->mItemBit & 0x7F, i_this->home.roomNo, 0, &angle, 1, NULL);
+    fopAcM_createItemFromTable(&i_this->mPos, i_this->mItemTable, i_this->mItemBit & 0x7F, fopAcM_GetHomeRoomNo(i_this), 0, &angle, 1, NULL);
     if (i_this->mType <= 1) {
         dComIfGp_particle_set(0x8168, &i_this->mPos);
         dComIfGp_particle_set(0x8167, &i_this->mPos, NULL, NULL, 255, NULL, -1, &i_this->tevStr.mColorK0, &i_this->tevStr.mColorK0);
         if (i_this->mType == 1) dComIfGp_particle_set(0x8169, &i_this->mPos, NULL, NULL, 255, NULL, -1, &i_this->tevStr.mColorK0, &i_this->tevStr.mColorK0);
     }
     if (i_this->mType == 2) dComIfGp_particle_set(0x816A, &i_this->mPos, NULL, NULL, 255, NULL, -1, &i_this->tevStr.mColorK0, &i_this->tevStr.mColorK0);
+#if VERSION > VERSION_DEMO
     fopAcM_seStart(i_this, JA_SE_OBJ_BREAK_CHAIR, 0);
+#endif
     fopAcM_create(fpcNm_Obj_Eff_e, 4, &i_this->mPos, -1, NULL, NULL, -1, NULL);
 }
 
@@ -212,6 +233,21 @@ static void cut_control2(sitem_class* i_this) {
     i_this->mModelAngle.x = -cM_atan2s(offset.y, offset.z);
     i_this->mModelAngle.y = cM_atan2s(offset.x, std::sqrtf(offset.y * offset.y + offset.z * offset.z));
     hand_mtx_set(i_this);
+#if VERSION == VERSION_DEMO
+    if (i_this->mpSmokeEmitter[1] == NULL && i_this->mSmokeActive) {
+        i_this->mSmoke[1].remove();
+        i_this->mpSmokeEmitter[1] = dComIfGp_particle_set(0x8184, &i_this->mSegments[0].pos, &i_this->mSmokeAngle[1], NULL, 255, &i_this->mSmoke[1], fopAcM_GetRoomNo(i_this));
+    } else {
+        offset = i_this->mSegments[0].pos - i_this->mSegments[1].pos;
+        i_this->mSmokeAngle[1].y = cM_atan2s(offset.x, offset.z);
+        i_this->mSmokeAngle[1].x = -cM_atan2s(offset.y, std::sqrtf(offset.x * offset.x + offset.z * offset.z));
+        if (i_this->mHitTimer == 1 && i_this->mpSmokeEmitter[1]) {
+            i_this->mpSmokeEmitter[1]->becomeInvalidEmitter();
+            i_this->mpSmokeEmitter[1] = NULL;
+            i_this->mSmokeActive = false;
+        }
+    }
+#else
     if (i_this->mSmoke[1].getEmitter() == NULL && i_this->mSmokeActive) {
         i_this->mSmoke[1].remove();
         dComIfGp_particle_set(0x8184, &i_this->mSegments[0].pos, &i_this->mSmokeAngle[1], NULL, 255, &i_this->mSmoke[1], fopAcM_GetRoomNo(i_this));
@@ -224,6 +260,7 @@ static void cut_control2(sitem_class* i_this) {
             i_this->mSmokeActive = false;
         }
     }
+#endif
 }
 
 static f32 hr_d[4] = {10.0f, 25.0f, 40.0f, 65.0f};
@@ -232,17 +269,19 @@ static f32 max_d[4] = {100.0f, 250.0f, 400.0f, 600.0f};
 /* 000015C0-00002304       .text hand_move__FP11sitem_class */
 static void hand_move(sitem_class* i_this) {
     fopAc_ac_c* actor = i_this;
-    f32 approach, length, lengthStep, wave;
+    f32 approach, maxSpeed, extension, length, lengthStep, wave;
     cXyz offset, direction, target, center;
     dBgS_GndChk ground;
     bool cut = false;
     approach = 0.1f;
+    maxSpeed = 8.0f;
+    extension = 0.0f;
     lengthStep = 1.0f;
     wave = 5.0f + REG0_F(11);
     length = REG0_F(5) + hr_d[i_this->mLengthType];
     f32 maxLength = max_d[i_this->mLengthType];
-    mDoMtx_YrotS(*calc_mtx, actor->current.angle.y);
-    mDoMtx_XrotM(*calc_mtx, actor->current.angle.x);
+    cMtx_YrotS(*calc_mtx, actor->current.angle.y);
+    cMtx_XrotM(*calc_mtx, actor->current.angle.x);
     switch (i_this->mMode) {
     case 0:
     case 1:
@@ -268,14 +307,18 @@ static void hand_move(sitem_class* i_this) {
         i_this->mAcch.CrrPos(*dComIfG_Bgsp());
         i_this->mHitTimer = 5;
         if (actor->speed.y < 0.0f) {
+#if VERSION == VERSION_DEMO
+            ground.m_pos.set(i_this->mPos.x, i_this->mPos.y + 100.0f, i_this->mPos.z);
+#else
             {
                 f32 y = i_this->mPos.y;
                 f32 z = i_this->mPos.z;
                 y += 100.0f;
                 ground.m_pos.set(i_this->mPos.x, y, z);
             }
+#endif
             i_this->mGroundY = dComIfG_Bgsp()->GroundCross(&ground);
-            if (i_this->mGroundY == -1000000000.0f || i_this->mPos.y <= 30.0f + i_this->mGroundY) {
+            if (i_this->mGroundY == -G_CM3D_F_INF || i_this->mPos.y <= 30.0f + i_this->mGroundY) {
                 i_this->mPos.y = 30.0f + i_this->mGroundY;
                 my_break(i_this);
             }
@@ -283,7 +326,7 @@ static void hand_move(sitem_class* i_this) {
         break;
     case 6:
         cut = true;
-        wave = 0.0f;
+        wave = extension;
         i_this->mHitTimer = 10;
         if (i_this->mTimers[0] < 40) {
             lengthStep = 0.05f * length;
@@ -293,12 +336,12 @@ static void hand_move(sitem_class* i_this) {
         break;
     }
     cLib_addCalc2(&i_this->mSegmentLength, length, 0.5f, lengthStep);
-    cLib_addCalc2(&i_this->mExtension, 0.0f, 1.0f, 0.2f);
+    cLib_addCalc2(&i_this->mExtension, extension, 1.0f, 0.2f);
     cLib_addCalc2(&i_this->mWave, wave, 1.0f, 1.5f);
     if (!cut) {
-        cLib_addCalc2(&actor->speedF, 8.0f, 1.0f, 0.1f);
+        cLib_addCalc2(&actor->speedF, maxSpeed, 1.0f, approach);
         if (i_this->mHitSpeed > 1.0f && i_this->mMode != 3) {
-            mDoMtx_YrotS(*calc_mtx, i_this->mHitAngle);
+            cMtx_YrotS(*calc_mtx, i_this->mHitAngle);
             offset.x = 0.0f;
             offset.y = REG6_F(9);
             offset.z = i_this->mHitSpeed;
@@ -312,6 +355,11 @@ static void hand_move(sitem_class* i_this) {
         cLib_addCalc2(&i_this->mPos.x, target.x, approach, actor->speedF);
         cLib_addCalc2(&i_this->mPos.y, target.y, approach, actor->speedF);
         cLib_addCalc2(&i_this->mPos.z, target.z, approach, actor->speedF);
+#if VERSION == VERSION_DEMO
+        if (cut) {
+            cLib_addCalcAngleS2(&actor->current.angle.y, fopAcM_searchActorAngleY(actor, dComIfGp_getPlayer(0)), 0x10, 0x800);
+        }
+#endif
         control1(i_this);
         control2(i_this);
     } else {
@@ -428,7 +476,7 @@ static BOOL daSitem_IsDelete(sitem_class*) {
 
 /* 000026FC-00002760       .text daSitem_Delete__FP11sitem_class */
 static BOOL daSitem_Delete(sitem_class* i_this) {
-    dComIfG_resDelete(&i_this->mPhase, "Sitem");
+    dComIfG_resDeleteDemo(&i_this->mPhase, "Sitem");
     i_this->mSmoke[0].remove();
     i_this->mSmoke[1].remove();
     return TRUE;
@@ -509,9 +557,10 @@ static cPhs_State daSitem_Create(fopAc_ac_c* actor) {
         }},
     };
     sitem_class* i_this = static_cast<sitem_class*>(actor);
-    fopAcM_SetupActor(actor, sitem_class);
+    fopAcM_SetupActor(i_this, sitem_class);
     cPhs_State phase = dComIfG_resLoad(&i_this->mPhase, "Sitem");
     if (phase == cPhs_COMPLEATE_e) {
+#if VERSION > VERSION_DEMO
         i_this->mType = fopAcM_GetParam(actor);
         if (i_this->mType == 255) i_this->mType = 0;
         i_this->mLengthType = fopAcM_GetParam(actor) >> 8;
@@ -519,8 +568,18 @@ static cPhs_State daSitem_Create(fopAc_ac_c* actor) {
         i_this->mItemTable = fopAcM_GetParam(actor) >> 24;
         if (i_this->mItemTable == 255) i_this->mItemTable = 0;
         i_this->mItemBit = fopAcM_GetParam(actor) >> 16;
+#endif
         if (fopAcM_entrySolidHeap(actor, daSitem_solidHeapCB, 0x3040)) {
             non_pos.set(0.0f, 30000.0f, -20000.0f);
+#if VERSION == VERSION_DEMO
+            i_this->mType = fopAcM_GetParam(actor);
+            if (i_this->mType == 255) i_this->mType = 0;
+            i_this->mLengthType = fopAcM_GetParam(actor) >> 8;
+            if (i_this->mLengthType > 3) i_this->mLengthType = 0;
+            i_this->mItemTable = fopAcM_GetParam(actor) >> 24;
+            if (i_this->mItemTable == 255) i_this->mItemTable = 0;
+            i_this->mItemBit = fopAcM_GetParam(actor) >> 16;
+#endif
             actor->health = 2;
             i_this->mFrame = cM_rndF(10000.0f);
             i_this->mStts.Init(255, 255, actor);
@@ -532,7 +591,7 @@ static cPhs_State daSitem_Create(fopAc_ac_c* actor) {
             i_this->mBoomerangSph.Set(bm_sph_src);
             i_this->mBoomerangSph.SetStts(&i_this->mStts);
             i_this->mPos = actor->current.pos;
-            i_this->mAcch.Set(&i_this->mPos, &i_this->mOldPos, actor, 1, &i_this->mAcchCir, &actor->speed, NULL, NULL);
+            i_this->mAcch.Set(&i_this->mPos, &i_this->mOldPos, actor, 1, &i_this->mAcchCir, fopAcM_GetSpeed_p(actor));
             i_this->mAcchCir.SetWall(30.0f, 50.0f);
             for (int i = 0; i < 2; i++) daSitem_Execute(i_this);
         } else phase = cPhs_ERROR_e;
