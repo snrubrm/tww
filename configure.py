52e448a0c4897f63b20b542bd1a2e854bf604fc6
snrubrm <snrubrm@proton.me>
2026-09-22 08:54:56 +0100

    configure: Mark newly matched units as matching for PAL

--- configure.py
+++ configure.py
@@ -1568,7 +1568,7 @@ config.libs = [
     ActorRel(Matching,    "d_a_himo3"),
     ActorRel(Matching,    "d_a_hmlif"),
     ActorRel(Matching,    "d_a_hys"),
-    ActorRel(MatchingFor("GZLE01") or Equivalent,  "d_a_kamome"),
+    ActorRel(MatchingFor("GZLE01", "GZLP01") or Equivalent,  "d_a_kamome"),
     ActorRel(Matching,    "d_a_kamome2"), # Demo-only TU
     ActorRel(MatchingFor("GZLE01", "GZLP01"), "d_a_kantera"),
     ActorRel(Matching,    "d_a_kn"),
