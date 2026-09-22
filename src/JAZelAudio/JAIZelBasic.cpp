6d8c6570d2a3c33b55a03b144d94804825bf6d1d
snrubrm <snrubrm@proton.me>
2026-09-22 20:46:40 +0100

    JAIZelBasic: Remove int copy of field_0x0207 in processLevObjSE

--- src/JAZelAudio/JAIZelBasic.cpp
+++ src/JAZelAudio/JAIZelBasic.cpp
@@ -3235,6 +3235,7 @@ void JAIZelBasic::setLevObjSE(u32 id, Vec* pos, s8 reverb) {
 }
 
 /* 802A8F58-802A90C0       .text processLevObjSE__11JAIZelBasicFv */
+// NONMATCHING - the target compares field_0x0207 with a signed cmpwi
 void JAIZelBasic::processLevObjSE() {
     for (u32 i = 0; i < mLevelSoundCount; i++) {
         f32 left = 0.0f;
@@ -3266,8 +3267,7 @@ void JAIZelBasic::processLevObjSE() {
 #if VERSION == VERSION_DEMO
         seStart(mLevelSounds[i].id, NULL, 0, reverb, 1.0f, volume, pan, rear, 1);
 #else
-        int muted = field_0x0207;
-        if (muted == 0) {
+        if (field_0x0207 == 0) {
             seStart(mLevelSounds[i].id, NULL, 0, reverb, 1.0f, volume, pan, rear, 1);
         }
 #endif
