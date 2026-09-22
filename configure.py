ea2d469f2eff2e12be5c1e3622cc9de13fb2981f
snrubrm <snrubrm@proton.me>
2026-09-22 18:52:42 +0100

    configure: Mark dolphin_trk_glue as matching for USA only

--- configure.py
+++ configure.py
@@ -1377,7 +1377,7 @@ config.libs = [
             Object(MatchingFor("GZLE01", "GZLP01", "GZLJ01", "D44J01"), "TRK_MINNOW_DOLPHIN/Os/dolphin/dolphin_trk.c"),
             Object(Matching,    "TRK_MINNOW_DOLPHIN/ppc/Generic/mpc_7xx_603e.c"),
             Object(Matching,    "TRK_MINNOW_DOLPHIN/Portable/main_TRK.c"),
-            Object(Matching, "TRK_MINNOW_DOLPHIN/Os/dolphin/dolphin_trk_glue.c"),
+            Object(MatchingFor("GZLE01"), "TRK_MINNOW_DOLPHIN/Os/dolphin/dolphin_trk_glue.c"),
             Object(Matching,    "TRK_MINNOW_DOLPHIN/Os/dolphin/targcont.c"),
             Object(Matching,    "TRK_MINNOW_DOLPHIN/Os/dolphin/target_options.c"),
             Object(Matching,    "TRK_MINNOW_DOLPHIN/MetroTRK/Export/mslsupp.c"),
