4000e53c7a5c97db1d287153a48b8ef0a65354b6
snrubrm <snrubrm@proton.me>
2026-09-22 12:07:19 +0100

    d_a_obj_tribox: Improve mode_block_walk (demo partial)

--- src/d/actor/d_a_obj_tribox.cpp
+++ src/d/actor/d_a_obj_tribox.cpp
@@ -659,9 +659,18 @@ void daObjTribox::Act_c::mode_block_walk() {
     bool done = --mTimer <= 0;
 
     f32 c = (f32)cos(0.157079637f * mTimer);
+#if VERSION == VERSION_DEMO
+    f32 rot0;
+    f32 rot1;
+    f32 s = (f32)mSign * (0.5f * (1.0f + c));
+    rot0 = 9.58738019e-5f * home.angle.y;
+    rot0 = 1.04719758f * mWalkRot + rot0;
+    rot1 = 1.04719758f * s;
+#else
     f32 rot0 = 9.58738019e-5f * home.angle.y;
     rot0 = 1.04719758f * mWalkRot + rot0;
     f32 rot1 = 1.04719758f * ((f32)mSign * (0.5f * (1.0f + c)));
+#endif
 
     MTXRotRad(mDoMtx_stack_c::now, 'Y', rot0);
     cXyz v0;
