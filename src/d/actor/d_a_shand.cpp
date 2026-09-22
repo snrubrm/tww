aa9fea127608b2d9aa5e237051ebdb32363518cf
snrubrm <snrubrm@proton.me>
2026-09-22 18:28:08 +0100

    d_a_shand: Fix demo profile status for linking (demo partial)

--- src/d/actor/d_a_shand.cpp
+++ src/d/actor/d_a_shand.cpp
@@ -549,7 +549,11 @@ actor_process_profile_definition g_profile_SHAND = {
     /* Leaf SubMtd  */ &g_fopAc_Method.base,
     /* Draw Prio    */ fpcDwPi_SHAND_e,
     /* Actor SubMtd */ &l_daShand_Method,
+#if VERSION == VERSION_DEMO
+    /* Status       */ fopAcStts_UNK40000_e,
+#else
     /* Status       */ fopAcStts_UNK4000_e | fopAcStts_UNK40000_e,
+#endif
     /* Group        */ fopAc_ENEMY_e,
     /* Cull Type    */ fopAc_CULLBOX_CUSTOM_e,
 };
