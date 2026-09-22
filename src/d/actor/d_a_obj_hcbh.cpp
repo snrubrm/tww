c1d28605d218c063e2aafeb06cac6fbb050280c2
snrubrm <snrubrm@proton.me>
2026-09-22 12:24:10 +0100

    d_a_obj_hcbh: Match leftover daObjHcbh_HIO_c, _create, _delete, _execute, wait_act_proc, make_item and more (demo partial)

--- src/d/actor/d_a_obj_hcbh.cpp
+++ src/d/actor/d_a_obj_hcbh.cpp
@@ -28,13 +28,66 @@ const Vec l_co_sph_offset[6] = {
 };
 }
 
+#if VERSION == VERSION_DEMO
+class daObjHcbh_HIO_c : public JORReflexible {
+public:
+    daObjHcbh_HIO_c();
+    virtual ~daObjHcbh_HIO_c() {}
+
+    void genMessage(JORMContext* ctx) { UNUSED(ctx); }
+
+public:
+    /* 0x04 */ s8 mNo;
+    /* 0x08 */ f32 mCylR;
+    /* 0x0C */ f32 mCylH;
+    /* 0x10 */ f32 mFallAccel;
+    /* 0x14 */ f32 mAngleSpeedBase;
+    /* 0x18 */ f32 mAngleSpeedScale;
+    /* 0x1C */ f32 mMaxAngle;
+    /* 0x20 */ f32 mGroundOffset;
+    /* 0x24 */ f32 mPieceAccel;
+    /* 0x28 */ f32 mPieceFloor;
+    /* 0x2C */ f32 mPieceTimerScale;
+    /* 0x30 */ f32 mPieceTimerBase;
+    /* 0x34 */ f32 mPieceAngleSpeedBase;
+    /* 0x38 */ f32 mPieceAngleSpeedScale;
+    /* 0x3C */ f32 m3C;
+    /* 0x40 */ u8 m40;
+    /* 0x41 */ u8 mParticle;
+    /* 0x42 */ u8 m42;
+};  // Size: 0x44
+
+/* 000000EC-0000018C       .text __ct__15daObjHcbh_HIO_cFv */
+daObjHcbh_HIO_c::daObjHcbh_HIO_c() {
+    mNo = -1;
+    mCylR = 70.0f;
+    mCylH = 589.0f;
+    mFallAccel = -0.011f;
+    mAngleSpeedBase = 4.0f;
+    mAngleSpeedScale = 127.0f;
+    mMaxAngle = 16384.0f;
+    mGroundOffset = -10.0f;
+    mPieceAccel = -0.01f;
+    mPieceFloor = -100.0f;
+    mPieceTimerScale = 15.0f;
+    mPieceTimerBase = 0.0f;
+    mPieceAngleSpeedBase = 4.0f;
+    mPieceAngleSpeedScale = 512.0f;
+    m3C = 8.0f;
+    m40 = 0;
+    mParticle = 1;
+    m42 = 0;
+}
+
+static daObjHcbh_HIO_c l_HIO;
+#endif
 
 /* 00000078-000000D8       .text chk_appear__11daObjHcbh_cFv */
 bool daObjHcbh_c::chk_appear() {
     s32 sw = param_get_swSave();
     bool appear = true;
     if (sw != 0xFF) {
-        appear = dComIfGs_isSwitch(sw, home.roomNo) == FALSE;
+        appear = fopAcM_isSwitch(this, sw) == FALSE;
     }
     return appear;
 }
@@ -79,7 +132,7 @@ bool daObjHcbh_c::create_heap() {
     bool success = true;
     J3DModelData* modelData = (J3DModelData*)dComIfG_getObjectRes(l_arcname, dRes_INDEX_HCBH_BDL_HCBH2_e);
     if (modelData == NULL) {
-        JUT_ASSERT(0x1f3, 0);
+        JUT_ASSERT(DEMO_SELECT(0x1F0, 0x1F3), 0);
         success = false;
     } else {
         mpModel = mDoExt_J3DModel__create(modelData, 0x80000, 0x11000022);
@@ -110,7 +163,11 @@ void daObjHcbh_c::setup_break_condition(fopAc_ac_c* actor) {
         for (int i = 0; i < 4; ++i) {
             mPieceYAngle[i] = set_anglY[i];
             mPieceAngle[i] = 0;
+#if VERSION == VERSION_DEMO
+            mPieceTimer[i] = l_HIO.mPieceTimerBase + l_HIO.mPieceTimerScale * std::fabsf(cM_ssin((set_anglY[i] - mBreakAngle) >> 1));
+#else
             mPieceTimer[i] = 15.0f * std::fabsf(cM_ssin((set_anglY[i] - mBreakAngle) >> 1));
+#endif
         }
     }
 }
@@ -172,8 +229,11 @@ void daObjHcbh_c::make_item() {
     cXyz pos = current.pos;
     csXyz angle(0, 0, 0);
     pos.y -= 30.0f;
-    fopAcM_createItemFromTable(&pos, item, saveBit, home.roomNo, 0, &angle, 1, NULL);
-    param_on_swSave();
+    fopAcM_createItemFromTable(&pos, item, saveBit, fopAcM_GetHomeRoomNo(this), 0, &angle, 1, NULL);
+    s32 sw = param_get_swSave();
+    if (sw != 0xFF) {
+        fopAcM_onSwitch(this, sw);
+    }
 }
 
 /* 00000A20-00000E30       .text _create__11daObjHcbh_cFv */
@@ -203,7 +263,7 @@ cPhs_State daObjHcbh_c::_create() {
                 fopAcM_SetMtx(this, mpModel->getBaseTRMtx());
                 init_mtx();
                 mAcchCir.SetWall(589.0f, 70.0f);
-                mAcch.Set(&current.pos, &old.pos, this, 1, &mAcchCir, &speed, &current.angle, &shape_angle);
+                mAcch.Set(fopAcM_GetPosition_p(this), fopAcM_GetOldPosition_p(this), this, 1, &mAcchCir, fopAcM_GetSpeed_p(this), fopAcM_GetAngle_p(this), fopAcM_GetShapeAngle_p(this));
                 mAcch.ClrWaterNone();
                 mAcch.ClrRoofNone();
                 mAcch.SetRoofCrrHeight(589.0f);
@@ -218,7 +278,11 @@ cPhs_State daObjHcbh_c::_create() {
                     mSph[i].Set(l_sph_src);
                     mSph[i].SetStts(&mStts);
                     mSph[i].SetR(70.0f);
+#if VERSION == VERSION_DEMO
+                    mSph[i].SetC(current.pos.x, current.pos.y + l_co_sph_offset[i].y, current.pos.z);
+#else
                     mSph[i].SetC(cXyz(current.pos.x, current.pos.y + l_co_sph_offset[i].y, current.pos.z));
+#endif
                 }
                 mSmoke.setTevStr(&tevStr);
                 mSmoke.setRateOff(0);
@@ -230,6 +294,11 @@ cPhs_State daObjHcbh_c::_create() {
             phase = cPhs_ERROR_e;
         }
     }
+#if VERSION == VERSION_DEMO
+    if (l_HIO.mNo < 0) {
+        l_HIO.mNo = mDoHIO_createChild("タートナックの柱", &l_HIO);
+    }
+#endif
     return phase;
 }
 
@@ -237,6 +306,14 @@ cPhs_State daObjHcbh_c::_create() {
 bool daObjHcbh_c::_delete() {
     if (mAppear == 1) {
         dComIfG_resDelete(&mPhase, l_arcname);
+#if VERSION == VERSION_DEMO
+        if (mpBgW != NULL && mpBgW->ChkUsed()) {
+            dComIfG_Bgsp()->Release(mpBgW);
+        }
+        if (mpPieceBgW != NULL && mpPieceBgW->ChkUsed()) {
+            dComIfG_Bgsp()->Release(mpPieceBgW);
+        }
+#else
         if (heap != NULL) {
             if (mpBgW != NULL) {
                 if (mpBgW->ChkUsed()) {
@@ -251,19 +328,36 @@ bool daObjHcbh_c::_delete() {
                 mpPieceBgW = NULL;
             }
         }
+#endif
         mSmoke.remove();
     }
+#if VERSION == VERSION_DEMO
+    if (l_HIO.mNo >= 0) {
+        mDoHIO_deleteChild(l_HIO.mNo);
+        l_HIO.mNo = -1;
+    }
+#endif
     return true;
 }
 
 /* 000012F4-000013BC       .text wait_act_proc__11daObjHcbh_cFv */
 void daObjHcbh_c::wait_act_proc() {
+#if VERSION == VERSION_DEMO
+    mCyl.SetR(l_HIO.mCylR);
+    mCyl.SetH(l_HIO.mCylH);
+#endif
     checkCollision();
     if (mBreakType == 0) {
         dComIfG_Ccsp()->Set(&mCyl);
     } else {
+#if VERSION == VERSION_DEMO
+        if (l_HIO.mParticle == 1) {
+            particle_set();
+        }
+#else
         particle_set();
-        mDoAud_seStart(JA_SE_OBJ_TN_COLUMN_BREAK, &current.pos, 0, dComIfGp_getReverb(current.roomNo));
+#endif
+        mDoAud_seStart(JA_SE_OBJ_TN_COLUMN_BREAK, &current.pos, 0, dComIfGp_getReverb(fopAcM_GetRoomNo(this)));
         mCyl.SetCoHitCallback(NULL);
         mAction = &daObjHcbh_c::fall_act_proc;
     }
@@ -283,12 +377,16 @@ void daObjHcbh_c::fall_act_proc() {
                 dComIfG_Bgsp()->Release(mpPieceBgW);
             }
         }
+#if VERSION == VERSION_DEMO
+        mAngleSpeed += (s16)(l_HIO.mAngleSpeedBase + l_HIO.mAngleSpeedScale * cM_ssin(mAngle));
+#else
         mAngleSpeed += (s16)(4.0f + 127.0f * cM_ssin(mAngle));
+#endif
     }
     if (mHeight < -10.0f + home.pos.y) {
         make_item();
         dComIfGp_getVibration().StartShock(8, -17, cXyz(0.0f, 1.0f, 0.0f));
-        mDoAud_seStart(JA_SE_OBJ_TN_COLUMN_SMASH, &current.pos, 0, dComIfGp_getReverb(current.roomNo));
+        mDoAud_seStart(JA_SE_OBJ_TN_COLUMN_SMASH, &current.pos, 0, dComIfGp_getReverb(fopAcM_GetRoomNo(this)));
         fopAcM_delete(this);
     } else {
         mFallSpeed += -0.011f;
@@ -311,12 +409,21 @@ void daObjHcbh_c::fall_act_proc() {
         if (mPieceTimer[i] > 0) {
             --mPieceTimer[i];
         } else {
+#if VERSION == VERSION_DEMO
+            mPieceSpeed[i] += l_HIO.mPieceAccel;
+            mPiecePos[i].y += mPieceSpeed[i];
+            if (mPiecePos[i].y < l_HIO.mPieceFloor + home.pos.y) {
+                mPiecePos[i].y = l_HIO.mPieceFloor + home.pos.y;
+            }
+            mPieceAngleSpeed[i] += (s16)(l_HIO.mPieceAngleSpeedBase + l_HIO.mPieceAngleSpeedScale * cM_ssin(mPieceAngle[i]));
+#else
             mPieceSpeed[i] += -0.01f;
             mPiecePos[i].y += mPieceSpeed[i];
             if (mPiecePos[i].y < -100.0f + home.pos.y) {
                 mPiecePos[i].y = -100.0f + home.pos.y;
             }
             mPieceAngleSpeed[i] += (s16)(4.0f + 512.0f * cM_ssin(mPieceAngle[i]));
+#endif
             mPieceAngle[i] += mPieceAngleSpeed[i];
             if (mPieceAngle[i] > 0x4000) {
                 mPieceAngle[i] = 0x4000;
@@ -338,9 +445,15 @@ bool daObjHcbh_c::_execute() {
     }
     mHeight += mFallSpeed;
     mAngle += mAngleSpeed;
+#if VERSION == VERSION_DEMO
+    if (mAngle >= (s16)l_HIO.mMaxAngle) {
+        mAngle = l_HIO.mMaxAngle;
+    }
+#else
     if (mAngle >= 0x4000) {
         mAngle = 0x4000;
     }
+#endif
     (this->*mAction)();
     return true;
 }
