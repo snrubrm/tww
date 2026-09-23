5bb7daa55ba8c43d7c58959bdb8231f05f28216f
snrubrm <snrubrm@proton.me>
2026-09-23 10:05:07 +0100

    d_a_mbdoor: Match leftover demoProc (USA partial)

--- src/d/actor/d_a_mbdoor.cpp
+++ src/d/actor/d_a_mbdoor.cpp
@@ -292,7 +292,7 @@ int daMbdoor_c::getDemoAction() {
 
 /* 00000B3C-00000F78       .text demoProc__10daMbdoor_cFv */
 void daMbdoor_c::demoProc() {
-    daPy_py_c* player = daPy_getPlayerActorClass();
+    daPy_py_c* player = (daPy_py_c*)dComIfGp_getPlayer(0);
     int actIdx = getDemoAction();
     cXyz goal;
     s16 angle;
