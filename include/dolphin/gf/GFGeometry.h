216342b4ffdb0a0e7bab0e52fc1dac4904c193bc
snrubrm <snrubrm@proton.me>
2026-09-24 15:16:58 +0100

    d_a_boomerang: Match leftover daBoomerang_blur_c::draw (demo partial)

--- include/dolphin/gf/GFGeometry.h
+++ include/dolphin/gf/GFGeometry.h
@@ -23,12 +23,18 @@ inline void GFBegin(GXPrimitive type, GXVtxFmt fmt, u16 vert_num) {
 
 inline void GFEnd() {}
 
+// GFWrite_f32/GFWrite_s16 go through GXCmd1u32/GXCmd1u16 like GFWrite_u8/u16 (sizes 0x30/0x2C in the demo debug map).
 inline void GFWrite_f32(f32 f) {
-    GX_WRITE_U32(*reinterpret_cast<u32*>(&f));
+    union {
+        f32 f;
+        u32 u;
+    } data;
+    data.f = f;
+    GXCmd1u32(data.u);
 }
 
 inline void GFWrite_s16(s16 s) {
-    GXFIFO.s16 = s;
+    GXCmd1u16(s);
 }
 
 inline void GFPosition3f32(f32 x, f32 y, f32 z) {
