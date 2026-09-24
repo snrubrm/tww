174da3be42faad825477a450a55c9d03d19e4222
snrubrm <snrubrm@proton.me>
2026-09-24 07:18:51 +0100

    d_a_pz: Reconstruct stripped debugDraw literal use

--- src/d/actor/d_a_pz.cpp
+++ src/d/actor/d_a_pz.cpp
@@ -2148,6 +2148,10 @@ void daPz_c::debugDraw() {
     pos.y += 20.0f;
     dLib_debugDrawFan(pos, shape_angle.y, l_HIO.m54, l_HIO.m58, color);
     dLib_debugDrawAxis(mWaistMtx, 50.0f);
+#if VERSION > VERSION_DEMO
+    // Stripped retail debug code left a 10000.0f literal; this statement is a reconstruction.
+    dLib_debugDrawAxis(mWaistMtx, 10000.0f);
+#endif
     GXColor c1 = {0xFF, 0xFF, 0x00, 0x80};
     GXColor c2 = {0xFF, 0x00, 0x00, 0x80};
     GXColor c3 = {0xFF, 0xFF, 0x00, 0x80};
@@ -2272,7 +2276,6 @@ void daPz_c::bodyDraw() {
 }
 
 /* 00006600-000066D8       .text drawShadow__6daPz_cFv */
-// NONMATCHING - retail .rodata offsets: the unreferenced 10000.0f that retail's (stripped) debugDraw left at 0x33C is missing
 void daPz_c::drawShadow() {
     cXyz pos(current.pos.x, current.pos.y + 100.0f + REG8_F(18), current.pos.z);
     mShadowId = dComIfGd_setShadow(
@@ -2347,7 +2350,6 @@ void daPz_c::bodyCreateInit() {
 }
 
 /* 00006974-00006BAC       .text createInit__6daPz_cFv */
-// NONMATCHING - retail .rodata offsets: the unreferenced 10000.0f that retail's (stripped) debugDraw left at 0x33C is missing
 void daPz_c::createInit() {
     static u8 fire_j[] = {7, 2, 12, 13, 17, 18, 22, 23, 25, 26};
     static f32 fire_sc[] = {2.0f, 2.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f};
