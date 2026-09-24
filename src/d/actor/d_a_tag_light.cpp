90dc7492b5a725c3d673800e9cf09f1c84172296
snrubrm <snrubrm@proton.me>
2026-09-24 08:56:33 +0100

    d_a_tag_light: Match leftover _draw (USA partial)

--- src/d/actor/d_a_tag_light.cpp
+++ src/d/actor/d_a_tag_light.cpp
@@ -506,10 +506,10 @@ bool Act_c::_draw() {
         }
 
         J3DModelData* modelData = mpModel->getModelData();
-        s8 signedAlpha = m308 * 255.5f;
-        u8 alpha = signedAlpha;
-        set_material(modelData->getJointNodePointer(MSPOT_JNT_HIKARITAG_e)->getMesh(), alpha);
-        set_material(modelData->getJointNodePointer(MSPOT_JNT_KOGEN_e)->getMesh(), alpha);
+        s8 tmp = (m308 * 255.5f);
+        u8 tmp2 = tmp;
+        set_material(modelData->getJointNodePointer(MSPOT_JNT_HIKARITAG_e)->getMesh(), (u8)tmp2);
+        set_material(modelData->getJointNodePointer(MSPOT_JNT_KOGEN_e)->getMesh(), (u8)tmp2);
         mDoExt_modelUpdateDL(mpModel);
     }
     return true;
